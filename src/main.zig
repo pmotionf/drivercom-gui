c0a978fd19eb32eafb0ede9025d0dfa88f65fe86
Jang-Junha618 <167044369+Jang-Junha618@users.noreply.github.com>
2024-09-25 18:06:32 +0900

    추가: config 파일 화면 표시

--- src/main.zig
+++ src/main.zig
@@ -9,8 +9,72 @@ const html = @embedFile("Index.html");
 const start_html = @embedFile("start.html");
 
 var config: drivercon.Config = undefined;
+var json: []u8 = undefined;
 
-fn fileOpen(allocator: std.mem.Allocator, file_name: []const u8) !void {
+var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+const allocator = arena.allocator();
+
+pub fn main() !void {
+    defer arena.deinit();
+
+    //config 파일을 json 형식으로 html에 전달
+    const j = try std.json.stringifyAlloc(allocator, config, .{});
+    defer allocator.free(j);
+    json = try allocator.alloc(u8, j.len + 1);
+    defer allocator.free(json);
+    @memcpy(json[0..j.len], j);
+    json[j.len] = 0;
+
+    const win = webui.newWindow();
+
+    _ = win.bind("showValue", showValue);
+    _ = win.bind("saveValue", saveValue);
+    _ = win.bind("selectFile", selectFile);
+
+    _ = win.show(html);
+
+    webui.wait();
+    webui.clean();
+}
+
+fn showValue(e: webui.Event) void {
+    const value = json[0 .. json.len - 1 :0];
+    // const value = config;
+    std.debug.print("Function called\n", .{});
+    e.returnString(value);
+}
+
+fn saveValue(e: webui.Event) void {
+    var response = std.mem.zeroes([64]u8);
+
+    const win = e.getWindow();
+    if (!win.script("return getValue();", 0, &response)) {
+        std.debug.print("get value {s}", .{response});
+    }
+    std.debug.print("Get value: {s}\n", .{response});
+}
+
+//파일 선택 > 열기
+fn selectFile(e: webui.Event) void {
+    var response = std.mem.zeroes([64]u8);
+
+    //선택한 파일 이름 받아오기
+    const win = e.getWindow();
+    if (!win.script("return fileName();", 0, &response)) {
+        std.debug.print("{s}", .{response});
+    }
+
+    //파일 열기
+    const file_name: []const u8 = removeZeros(response);
+    fileOpen(file_name) catch |err| {
+        std.log.err("{any}", .{err});
+        return;
+    };
+    std.debug.print("{any}", .{config.id}); //>>테스트,  삭제 예정
+}
+
+//파일 열기
+fn fileOpen(file_name: []const u8) !void {
     var file = try std.fs.cwd().openFile(file_name, .{});
     defer file.close();
 
@@ -22,67 +86,30 @@ fn fileOpen(allocator: std.mem.Allocator, file_name: []const u8) !void {
     );
     defer untyped.deinit();
     config = try untyped.parse(drivercon.Config);
+
+    //config 파일을 json 형식으로 html에 전달
+    allocator.free(json);
+    const j = try std.json.stringifyAlloc(allocator, config, .{});
+    defer allocator.free(j);
+    allocator.free(json);
+    json = try allocator.alloc(u8, j.len + 1);
+    @memcpy(json[0..j.len], j);
+    json[j.len] = 0;
 }
 
-pub fn main() !void {
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
-    const allocator = arena.allocator();
+//문자열에서 0제거하기
+fn removeZeros(input: [64]u8) []u8 {
+    var buffer: [64]u8 = undefined;
+    var index: usize = 0;
 
-    //yaml 파일 열기
-    try fileOpen(allocator, "config.yml");
-
-    std.log.info("{}", .{config.station_id});
-    const info = @typeInfo(drivercon.Config).@"struct".fields;
-
-    inline for (info, 0..) |con, i| {
-        const value = @field(config, info[i].name);
-
-        std.debug.print("{s} : {any} \n", .{ con.name, value });
-    }
-
-    //디렉토리 읽어오기
-    const cwd = std.fs.cwd();
-    var dir = try cwd.openDir(".", .{ .iterate = true });
-    defer dir.close();
-    var iterate = dir.iterate();
-
-    var file_names = std.ArrayList([]const u8).init(allocator);
-    defer file_names.deinit();
-
-    while (try iterate.next()) |next_entry| {
-        if (std.mem.endsWith(u8, next_entry.name, ".yml")) {
-            try file_names.append(next_entry.name);
-            std.debug.print("\nname : {s}", .{next_entry.name});
+    for (input) |char| {
+        if (char != 0) {
+            buffer[index] = char;
+            index += 1;
         }
     }
-    std.debug.print("\nfiles : {s}", .{file_names.items});
 
-    const win = webui.newWindow();
-
-    _ = win.bind("showValue", showValue);
-    _ = win.bind("saveValue", saveValue);
-
-    _ = win.show(start_html);
-
-    webui.wait();
-    webui.clean();
-}
-
-fn showValue(e: webui.Event) void {
-    const value = config.id;
-    std.debug.print("Function called\n", .{});
-    e.returnValue(value);
-}
-
-fn saveValue(e: webui.Event) void {
-    var response = std.mem.zeroes([64]u8);
-
-    const win = e.getWindow();
-    if (!win.script("return getValue();", 0, &response)) {
-        std.debug.print("get value {s}", .{response});
-    }
-    std.debug.print("Get value: {s}\n", .{response});
+    return buffer[0..index];
 }
 
 fn exit(_: webui.Event) void {
