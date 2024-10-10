7cb9ef5a52b1773018ba9989a2a9378658da56f4
mochalins <117967760+mochalins@users.noreply.github.com>
2024-10-10 10:45:19 +0900

    scm: Prettier formatting

--- publish.js
+++ publish.js
@@ -1,19 +1,19 @@
-module.exports = async ({github, context}) => {
-  const {SHA} = process.env;
-  const fs = require('fs').promises;
-  const org = 'pmotionf';
-  const name = 'drivercom-gui';
+module.exports = async ({ github, context }) => {
+  const { SHA } = process.env;
+  const fs = require("fs").promises;
+  const org = "pmotionf";
+  const name = "drivercom-gui";
 
   // Parse `build.zig.zon` for version
   var version;
-  const raw = await fs.readFile('build.zig.zon');
-  const lines = raw.split('\n');
+  const raw = await fs.readFile("build.zig.zon");
+  const lines = raw.split("\n");
   lines.forEach((line) => {
     line = line.trim();
-    if (line.startsWith('.version')) {
-      const parts = line.split('=');
+    if (line.startsWith(".version")) {
+      const parts = line.split("=");
       const version_raw = parts[1];
-      const version_cleaned = version_raw.replace('"', '').replace(',', '');
+      const version_cleaned = version_raw.replace('"', "").replace(",", "");
       version = version_cleaned.trim();
     }
   });
@@ -22,7 +22,7 @@ module.exports = async ({github, context}) => {
   const releases = await github.rest.git.listMatchingRefs({
     owner: org,
     repo: name,
-    ref: 'tags/' + version,
+    ref: "tags/" + version,
   });
   if (releases.length > 0) return;
 
@@ -35,22 +35,28 @@ module.exports = async ({github, context}) => {
     generate_release_notes: true,
   });
 
-  github.request('POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}', {
-    owner: org,
-    repo: name,
-    release_id: version,
-    data: await fs.readFile('zig-out/bin/' + name),
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  });
-  github.request('POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}', {
-    owner: org,
-    repo: name,
-    release_id: version,
-    data: await fs.readFile('zig-out/bin/' + name + '.exe'),
-    headers: {
-      'X-GitHub-Api-Version': '2022-11-28'
-    }
-  });
-}
+  github.request(
+    "POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}",
+    {
+      owner: org,
+      repo: name,
+      release_id: version,
+      data: await fs.readFile("zig-out/bin/" + name),
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    },
+  );
+  github.request(
+    "POST /repos/{owner}/{repo}/releases/{release_id}/assets{?name,label}",
+    {
+      owner: org,
+      repo: name,
+      release_id: version,
+      data: await fs.readFile("zig-out/bin/" + name + ".exe"),
+      headers: {
+        "X-GitHub-Api-Version": "2022-11-28",
+      },
+    },
+  );
+};
