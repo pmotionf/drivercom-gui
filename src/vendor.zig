635fb93a8512a6a4dcbc86294931cf8c49218fd6
kmtso434 <hakim@pmotionf.com>
2024-10-11 17:52:03 +0900

    수정:jquery vendor 폴더에 추가

--- src/vendor.zig
+++ src/vendor.zig
@@ -1,2 +1,3 @@
 pub const @"dygraph.min.js" = @embedFile("vendor/dygraph.min.js");
 pub const @"synchronizer.js" = @embedFile("vendor/synchronizer.js");
+pub const @"jquery-3.6.0.min.js" = @embedFile("vendor/jquery-3.6.0.min.js");
