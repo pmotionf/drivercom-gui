4653baf0f05222beee5d8cd2d27da4ce97b3e342
mochalins <117967760+mochalins@users.noreply.github.com>
2024-10-13 14:21:01 +0900

    feat: Collapsed sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,12 @@ import "./App.css";
 import { Index, createSignal, onMount } from "solid-js";
 import { A, useNavigate } from "@solidjs/router";
 
-import { IconMenu, IconChevronLeftPipe } from "@tabler/icons-solidjs";
+import {
+  IconChevronLeftPipe,
+  IconFileSettings,
+  IconGraph,
+  IconMenu,
+} from "@tabler/icons-solidjs";
 
 import { Button } from "~/components/ui/button";
 import { Drawer } from "~/components/ui/drawer";
@@ -22,86 +27,141 @@ function App(props) {
   });
 
   const navigate = useNavigate();
-  const pages = ["Configuration", "Logging"];
+  const pages = {
+    configuration: {
+      icon: () => <IconFileSettings />,
+      label: "Configuration",
+    },
+    logging: {
+      icon: () => <IconGraph />,
+      label: "Logging",
+    },
+  };
   const [page, setPage] = createSignal("");
 
   return (
     <>
-      <Drawer.Root variant="left">
-        <Drawer.Trigger
-          asChild={(triggerProps) => (
-            <Button {...triggerProps()}>
-              <IconMenu />
-            </Button>
-          )}
-        />
-        <Drawer.Backdrop />
-        <Drawer.Positioner style={{ width: "30%", "max-width": "18em" }}>
-          <Drawer.Content>
-            <Drawer.Header>
-              <Drawer.Title style={{ "padding-top": "0px" }}>
-                Drivercom
-              </Drawer.Title>
-              <Drawer.CloseTrigger
-                asChild={(closeProps) => (
-                  <Button
-                    {...closeProps()}
-                    variant="ghost"
-                    style={{
-                      position: "absolute",
-                      top: "0px",
-                      right: "0px",
-                      padding: "0px",
-                    }}
-                  >
-                    <IconChevronLeftPipe />
-                  </Button>
-                )}
-              />
-            </Drawer.Header>
-            <Drawer.Body
-              style={{
-                width: "100%",
-                "padding-top": "0.5em",
-                "padding-left": "0.5em",
-                "padding-right": "0px",
-                "padding-bottom": "0px",
-              }}
-            >
-              <SegmentGroup.Root
-                value={page()}
-                onValueChange={(e) => {
-                  setPage(e.value);
-                  navigate("/" + e.value.toLowerCase(), { replace: true });
+      <div style={{ width: "3em", height: "100%", position: "fixed" }}>
+        <Drawer.Root variant="left">
+          <Drawer.Trigger
+            asChild={(triggerProps) => (
+              <Button
+                {...triggerProps()}
+                style={{
+                  "border-radius": "0px",
+                  margin: "0px",
+                  width: "100%",
+                  padding: "0px",
                 }}
-                style={{ width: "98%" }}
               >
-                <Index each={pages}>
-                  {(val) => (
-                    <SegmentGroup.Item value={val()} style={{ width: "100%" }}>
-                      <SegmentGroup.ItemText>{val()}</SegmentGroup.ItemText>
-                      <SegmentGroup.ItemControl />
-                      <SegmentGroup.ItemHiddenInput />
-                    </SegmentGroup.Item>
+                <IconMenu />
+              </Button>
+            )}
+          />
+          <Drawer.Backdrop />
+          <Drawer.Positioner style={{ width: "30%", "max-width": "18em" }}>
+            <Drawer.Content>
+              <Drawer.Header>
+                <Drawer.Title style={{ "padding-top": "0px" }}>
+                  Drivercom
+                </Drawer.Title>
+                <Drawer.CloseTrigger
+                  asChild={(closeProps) => (
+                    <Button
+                      {...closeProps()}
+                      variant="ghost"
+                      style={{
+                        position: "absolute",
+                        top: "0px",
+                        right: "0px",
+                        padding: "0px",
+                      }}
+                    >
+                      <IconChevronLeftPipe />
+                    </Button>
                   )}
-                </Index>
-                <SegmentGroup.Indicator />
-              </SegmentGroup.Root>
-            </Drawer.Body>
-            <Drawer.Footer>
-              <Text
-                as="span"
-                size="s"
-                fontWeight="light"
-                color="{colors.gray.a8}"
+                />
+              </Drawer.Header>
+              <Drawer.Body
+                style={{
+                  width: "100%",
+                  "padding-top": "0.5em",
+                  "padding-left": "0.5em",
+                  "padding-right": "0px",
+                  "padding-bottom": "0px",
+                }}
               >
-                <i>Copyright © 2024 PMF, Inc.</i>
-              </Text>
-            </Drawer.Footer>
-          </Drawer.Content>
-        </Drawer.Positioner>
-      </Drawer.Root>
-      {props.children}
+                <SegmentGroup.Root
+                  value={page()}
+                  onValueChange={(e) => {
+                    setPage(e.value);
+                    navigate("/" + e.value.toLowerCase(), { replace: true });
+                  }}
+                  style={{ width: "98%" }}
+                >
+                  <Index each={Object.keys(pages)}>
+                    {(val) => (
+                      <SegmentGroup.Item
+                        value={val()}
+                        style={{ width: "100%" }}
+                      >
+                        <SegmentGroup.ItemText>
+                          <span
+                            style={{ float: "left", "padding-right": "0.2em" }}
+                          >
+                            {pages[val()].icon}
+                          </span>
+                          <span style={{ float: "left" }}>
+                            {pages[val()].label}
+                          </span>
+                        </SegmentGroup.ItemText>
+                        <SegmentGroup.ItemControl />
+                        <SegmentGroup.ItemHiddenInput />
+                      </SegmentGroup.Item>
+                    )}
+                  </Index>
+                  <SegmentGroup.Indicator />
+                </SegmentGroup.Root>
+              </Drawer.Body>
+              <Drawer.Footer>
+                <Text
+                  as="span"
+                  size="s"
+                  fontWeight="light"
+                  color="{colors.gray.a8}"
+                >
+                  <i>Copyright © 2024 PMF, Inc.</i>
+                </Text>
+              </Drawer.Footer>
+            </Drawer.Content>
+          </Drawer.Positioner>
+        </Drawer.Root>
+        <SegmentGroup.Root
+          value={page()}
+          onValueChange={(e) => {
+            setPage(e.value);
+            navigate("/" + e.value.toLowerCase(), { replace: true });
+          }}
+          style={{ position: "fixed", height: "100%", width: "3em" }}
+          background="{colors.gray.3}"
+        >
+          <Index each={Object.keys(pages)}>
+            {(val) => (
+              <SegmentGroup.Item value={val()}>
+                <SegmentGroup.ItemText>
+                  {pages[val()].icon}
+                </SegmentGroup.ItemText>
+                <SegmentGroup.ItemControl />
+                <SegmentGroup.ItemHiddenInput />
+              </SegmentGroup.Item>
+            )}
+          </Index>
+          <SegmentGroup.Indicator />
+        </SegmentGroup.Root>
+      </div>
+      <div style={{ width: "calc(100% - 3em)", "margin-left": "3em" }}>
+        {props.children}
+      </div>
     </>
   );
 }
