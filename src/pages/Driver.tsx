60d2fe5ea154cfcf22c840e5354b231c746d030c
Jang-Junha618 <167044369+Jang-Junha618@users.noreply.github.com>
2024-10-28 14:59:33 +0900

    추가: drivercom  각 기능별 정보 출력

--- src/pages/Driver.tsx
+++ src/pages/Driver.tsx
@@ -1,21 +1,68 @@
-import { createSignal, Show } from "solid-js";
+import { createSignal, Show, For } from "solid-js";
 import { Button } from "~/components/ui/button";
+import { Text } from "~/components/ui/text";
 import { Command } from "@tauri-apps/plugin-shell";
 
 function Connect() {
   const [portList, setPortList] = createSignal("");
-  const [porttErr, setPortErr] = createSignal("");
+  const [stdErr, setstdtErr] = createSignal("");
+  const [buttonList, setButtonList] = createSignal<string[]>([]);
+  const [buttonClicked, setButtonClicked] = createSignal<boolean[]>([]);
+  const [help, setHelp] = createSignal("");
+  const commands = ["version", "firmware", "config", "log"];
+
   async function connectPort() {
     const drivercom = Command.sidecar("binaries/drivercom", ["port.list"]);
     const output = await drivercom.execute();
+    const btn_list = output.stdout.match(/\((\w+)\)/g) as string[];
+    setButtonList(btn_list);
     setPortList(output.stdout);
-    setPortErr(output.stderr);
+    setstdtErr(output.stderr);
+    setButtonClicked(btn_list.map(() => false));
   }
+
+  function portClick(index: number) {
+    setButtonClicked((prev) => {
+      const updated = [...prev];
+      updated[index] = true;
+      return updated;
+    });
+  }
+
+  async function commandClick(index: number) {
+    const drivercom = Command.sidecar("binaries/drivercom", [commands[index]]);
+    const output = await drivercom.execute();
+    setHelp(output.stdout);
+    console.log(output.stderr);
+  }
+
   return (
     <>
       <Button onclick={connectPort}>connect</Button>
-      <Show when={portList() != "" && porttErr() == ""}>
-        <Button>{portList()}</Button>
+      <Show when={portList() != "" && stdErr() == ""}>
+        <Text>{buttonList()}</Text>
+        <For each={buttonList()}>
+          {(name, index) => (
+            <>
+              <Button onclick={() => portClick(index())}>
+                {name.slice(1, -1)}
+              </Button>{" "}
+              <br />
+              <Show when={buttonClicked()[index()]}>
+                <For each={commands}>
+                  {(item, index) => (
+                    <>
+                      <Button onclick={() => commandClick(index())}>
+                        {item}
+                      </Button>
+                    </>
+                  )}
+                </For>
+                <Text>{help()}</Text>
+              </Show>
+            </>
+          )}
+        </For>
       </Show>
     </>
   );
