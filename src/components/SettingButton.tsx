a1fd801d222b5d76b892c88d737a0cae72d76e13
kmtso434 <122084476+kmtso434@users.noreply.github.com>
2024-10-29 17:28:11 +0900

    추가 : 차트 선 점/선 변경기능 추가

--- src/components/SettingButton.tsx
+++ src/components/SettingButton.tsx
@@ -4,43 +4,87 @@ import { IconButton } from "~/components/ui/icon-button";
 import { Button } from "~/components/ui/button";
 import { Text } from "~/components/ui/text";
 import { Input } from "~/components/ui/input";
-import { IconSettings } from "@tabler/icons-solidjs";
+import {
+  IconLine,
+  IconLineDashed,
+  IconPoint,
+  IconSettings,
+} from "@tabler/icons-solidjs";
 import { Portal, render } from "solid-js/web";
 import { Dialog } from "~/components/ui/dialog";
 import { parseColor } from "@ark-ui/solid";
 import { Stack } from "styled-system/jsx";
 import { JSX, createSignal } from "solid-js";
-import { Heading } from "./ui/heading";
+import { ToggleGroup } from "~/components/ui/toggle-group";
 
 export type SettingButtonListProps = JSX.HTMLAttributes<HTMLElement> & {
-    uplotId : string;
-    uplot: PlotContainer;
-    index: number;
-}
+  uplotId: string;
+  uplot: PlotContainer;
+  index: number;
+};
 
 type PlotContainer = {
-    plot: uPlot | null;
-}
+  plot: uPlot | null;
+};
 
-export function SettingButton (props: SettingButtonListProps) {
+export function SettingButton(props: SettingButtonListProps) {
   const [color, setColor] = createSignal("");
+  const [style, setStyle] = createSignal("");
+  const [strokeWidth, setStrokeWidth] = createSignal(
+    props.uplot.plot!.series[props.index].width,
+  );
   const div = document.getElementById(props.uplotId + "-wrapper");
   const markers = div!.querySelectorAll(`.u-marker`);
   const marker = markers.item(props.index) as HTMLElement;
   const prevStrokeColor = getComputedStyle(marker).borderColor;
   setColor(prevStrokeColor);
 
-  const changeColor = (index: number) => {
+  var dash = props.uplot.plot!.series[props.index].dash;
+  if (dash === undefined) setStyle("line") && dash === undefined;
+  else if (JSON.stringify(dash) === JSON.stringify([0, 5])) setStyle("dot");
+  else setStyle("dottedLine");
+
+  const changeStyle = (index: number) => {
     if (props.uplot.plot) {
-    var series = props.uplot.plot!.series[index];
-    props.uplot.plot.delSeries(index);
-    props.uplot.plot.addSeries(
-        { stroke: color(), label: series.label! },
-        index,
-    );
-    props.uplot.plot.redraw();
-    addOptionButton(props.uplotId, props.uplot, index);
+      var series = props.uplot.plot!.series[index];
+      props.uplot.plot.delSeries(index);
+      if (style() === "line") {
+        props.uplot.plot.addSeries(
+          {
+            stroke: color(),
+            label: series.label!,
+            width: strokeWidth(),
+          },
+          index,
+        );
+      } else if (style() === "dot") {
+        props.uplot.plot.addSeries(
+          {
+            stroke: color(),
+            label: series.label!,
+            dash: [0, 5],
+            width: strokeWidth(),
+          },
+          index,
+        );
+      } else if (style() === "dottedLine") {
+        props.uplot.plot.addSeries(
+          {
+            stroke: color(),
+            label: series.label!,
+            dash: [20, 10],
+            width: strokeWidth(),
+          },
+          index,
+        );
+      }
+      props.uplot.plot.redraw();
+      addOptionButton(props.uplotId, props.uplot, index);
     }
+  };
+
+  const inputHandler = (event: any) => {
+    setStrokeWidth(parseInt(event.currentTarget.value));
   };
 
   return (
@@ -74,73 +118,121 @@ export function SettingButton (props: SettingButtonListProps) {
       <Portal>
         <Dialog.Backdrop />
         <Dialog.Positioner>
-          <Dialog.Content>
-            <Stack gap="8" p="6">
-              <ColorPicker.Root
-                value={parseColor(color())}
-                onValueChange={(e) => setColor(e.valueAsString)}
-                onValueChangeEnd={(e) => setColor(e.valueAsString)}
-                open={true}
+          <Dialog.Content p="6">
+            <ColorPicker.Root
+              value={parseColor(color())}
+              onValueChange={(e) => setColor(e.valueAsString)}
+              onValueChangeEnd={(e) => setColor(e.valueAsString)}
+              open={true}
+            >
+              <ColorPicker.Label>
+                <Text
+                  size="xs"
+                  fontWeight="bold"
+                  style={{ "margin-top": "0.2rem" }}
                 >
-                <ColorPicker.Label>
-                  <Heading>Color</Heading>
-                </ColorPicker.Label>
-                <ColorPicker.Control>
+                  Stroke Color
+                </Text>
+              </ColorPicker.Label>
+              <ColorPicker.Control>
                 <ColorPicker.ChannelInput
-                    channel="hex"
-                    asChild={(inputProps) => <Input {...inputProps()} />}
+                  channel="hex"
+                  asChild={(inputProps) => <Input {...inputProps()} />}
                 />
                 <IconButton variant="outline">
                   <ColorPicker.ValueSwatch />
                 </IconButton>
-                </ColorPicker.Control>
-                  <ColorPicker.Area>
-                    <ColorPicker.AreaBackground />
-                    <ColorPicker.AreaThumb />
-                  </ColorPicker.Area>
-                <ColorPicker.ChannelSlider
+              </ColorPicker.Control>
+              <ColorPicker.Area mt="1">
+                <ColorPicker.AreaBackground />
+                <ColorPicker.AreaThumb />
+              </ColorPicker.Area>
+              <ColorPicker.ChannelSlider
                 channel="hue"
                 style={{ "margin-top": "1rem" }}
-                >
-                    <ColorPicker.ChannelSliderTrack />
-                    <ColorPicker.ChannelSliderThumb />
-                </ColorPicker.ChannelSlider>
-                <Text
-                  size="xs"
-                  fontWeight="bold"
-                  color="fg.default"
-                  style={{ "margin-top": "0.2rem" }}
-                >
-                  Color Palette
-                </Text>
-                <ColorPicker.SwatchGroup>
-                  <For each={presets}>
-                    {(color) => (
+              >
+                <ColorPicker.ChannelSliderTrack />
+                <ColorPicker.ChannelSliderThumb />
+              </ColorPicker.ChannelSlider>
+              <Text
+                size="xs"
+                fontWeight="bold"
+                style={{ "margin-top": "0.2rem" }}
+              >
+                Color Palette
+              </Text>
+              <ColorPicker.SwatchGroup>
+                <For each={presets}>
+                  {(color) => (
                     <ColorPicker.SwatchTrigger value={color}>
                       <ColorPicker.Swatch value={color} />
                     </ColorPicker.SwatchTrigger>
-                    )}
-                  </For>
-                </ColorPicker.SwatchGroup>
-              </ColorPicker.Root>
+                  )}
+                </For>
+              </ColorPicker.SwatchGroup>
+            </ColorPicker.Root>
+            <Stack>
+              <Stack direction="row" mt="2" width="full">
+                <Text size="xs" fontWeight="bold">
+                  Stroke Width
+                </Text>
+                <Text
+                  size="xs"
+                  fontWeight="bold"
+                  style={{ "margin-left": "10rem" }}
+                >
+                  Stroke Style
+                </Text>
+              </Stack>
+              <Stack direction="row" width={"23rem"}>
+                <Input
+                  id={"inputStrokeWidth"}
+                  value={strokeWidth()}
+                  width="full"
+                  placeholder="width"
+                  type="number"
+                  max={10}
+                  min={1}
+                  onInput={(e) => inputHandler(e)}
+                />
+                <ToggleGroup.Root value={[style()]} width={"12rem"}>
+                  <ToggleGroup.Item
+                    value={"line"}
+                    onClick={() => setStyle("line")}
+                  >
+                    <IconLine />
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item
+                    value={"dot"}
+                    onClick={() => setStyle("dot")}
+                  >
+                    <IconPoint />
+                  </ToggleGroup.Item>
+                  <ToggleGroup.Item
+                    value={"dottedLine"}
+                    onClick={() => setStyle("dottedLine")}
+                  >
+                    <IconLineDashed />
+                  </ToggleGroup.Item>
+                </ToggleGroup.Root>
+              </Stack>
               <Dialog.CloseTrigger>
-                <Stack direction="row" width="full">
-                    <Button
-                        variant={"outline"}
-                        width={"full"}
-                        onClick={()=> {setColor(prevStrokeColor)
-                        }}
-                    >
-                        Cancel
-                    </Button>
-                    <Button
-                        width={"full"}
-                        onClick={() => {
-                        changeColor(props.index);
-                        }}
-                    >
-                        Save
-                    </Button>
+                <Stack direction="row" width="full" mt="1">
+                  <Button
+                    variant={"outline"}
+                    width={"full"}
+                    onClick={() => {
+                      setColor(prevStrokeColor);
+                    }}
+                  >
+                    Cancel
+                  </Button>
+                  <Button
+                    width={"full"}
+                    onClick={() => changeStyle(props.index)}
+                  >
+                    Save
+                  </Button>
                 </Stack>
               </Dialog.CloseTrigger>
             </Stack>
@@ -151,37 +243,33 @@ export function SettingButton (props: SettingButtonListProps) {
   );
 }
 
-function addOptionButton(
-    uplotId: string,
-    uplot: PlotContainer,
-    index: number,
-){
-    const div = document.getElementById(uplotId + "-wrapper");
-    if (div) {
-        const legend_elements = div.querySelectorAll(`.u-series`);
-        var row = legend_elements.item(index) as HTMLTableRowElement;
-        var new_cell = row.insertCell(0);
-        const container = document.createElement("div");
-        new_cell.appendChild(container);
-        render(
-            () => <SettingButton uplotId={uplotId} uplot={uplot} index={index} />,
-            container
-        )
-    }
+function addOptionButton(uplotId: string, uplot: PlotContainer, index: number) {
+  const div = document.getElementById(uplotId + "-wrapper");
+  if (div) {
+    const legend_elements = div.querySelectorAll(`.u-series`);
+    var row = legend_elements.item(index) as HTMLTableRowElement;
+    var new_cell = row.insertCell(0);
+    const container = document.createElement("div");
+    new_cell.appendChild(container);
+    render(
+      () => <SettingButton uplotId={uplotId} uplot={uplot} index={index} />,
+      container,
+    );
+  }
 }
 
 const presets = [
-    "#820D37", //more purplered
-    "#B32357", //purple red
-    "#CE2E68", //pink red
-    "#E65A00", //orange
-    "#FD8305", //yellow orange
-    "#FFC107", //more yellow orange
-    "#FFEC07", //yellow
-    "#78B398", //pastel dark green
-    "#009E73", //blue green
-    "#14C7BA", //mint
-    "#449AE4", //skyblue
-    "#3B3EDE", //dark blue
-    "#2C2E9C", //more dark blue
-  ];
+  "#820D37", //more purplered
+  "#B32357", //purple red
+  "#CE2E68", //pink red
+  "#E65A00", //orange
+  "#FD8305", //yellow orange
+  "#FFC107", //more yellow orange
+  "#FFEC07", //yellow
+  "#78B398", //pastel dark green
+  "#009E73", //blue green
+  "#14C7BA", //mint
+  "#449AE4", //skyblue
+  "#3B3EDE", //dark blue
+  "#2C2E9C", //more dark blue
+];
