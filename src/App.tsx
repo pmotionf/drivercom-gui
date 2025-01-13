963535750ffef3276caefd5107543d51d35e16cb
kmtso434 <122084476+kmtso434@users.noreply.github.com>
2025-01-13 17:36:08 +0900

    feat: Log configure page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,14 @@
 import "./App.css";
 
 import { invoke } from "@tauri-apps/api/core";
-import {
-  createSignal,
-  Index,
-  onMount,
-  Show,
-  ValidComponent,
-} from "solid-js";
+import { createSignal, Index, onMount, Show, ValidComponent } from "solid-js";
 import { Dynamic, Portal } from "solid-js/web";
 import type { RouteSectionProps } from "@solidjs/router";
 import { useNavigate } from "@solidjs/router";
 
 import {
   IconChevronLeftPipe,
+  IconFileAnalytics,
   IconFileSettings,
   IconGraph,
   IconMenu,
@@ -77,6 +72,11 @@ function App(props: RouteSectionProps) {
     connect: {
       icon: IconPlugConnected,
       label: "Connect",
+      disabled: false,
+    },
+    logConfigure: {
+      icon: IconFileAnalytics,
+      label: "Log Configure",
       disabled: false,
     },
   };
--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,7 @@ import App from "./App";
 const Configuration = lazy(() => import("./pages/Configuration"));
 const Logging = lazy(() => import("./pages/Logging"));
 const Connect = lazy(() => import("./pages/Connect"));
+const LogConfigure = lazy(() => import("./pages/LogConfigure"));
 
 render(
   () => (
@@ -15,6 +16,7 @@ render(
       <Route path="/configuration" component={Configuration} />
       <Route path="/logging" component={Logging} />
       <Route path="/connect" component={Connect} />
+      <Route path="/logConfigure" component={LogConfigure} />
     </Router>
   ),
   document.getElementById("root") as HTMLElement,
