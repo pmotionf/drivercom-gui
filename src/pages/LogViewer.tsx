f9a9a3cbfa821eea1dc72e700726ab0bee5d96d9
kmtso434 <122084476+kmtso434@users.noreply.github.com>
2025-02-14 17:36:06 +0900

    feat: Move tab data to other tab

--- src/pages/LogViewer.tsx
+++ src/pages/LogViewer.tsx
@@ -1,59 +1,202 @@
 import { Stack } from "styled-system/jsx";
-import {LogViewerTabList} from "./LogViewer/LogViewerTabList";
 import { Splitter } from "~/components/ui/splitter";
-import { createEffect, createSignal, Show } from "solid-js";
+import { createSignal, Show } from "solid-js";
+import { LogViewerTabList } from "./LogViewer/LogViewerTabList";
+import { open } from "@tauri-apps/plugin-dialog";
+import { Toast } from "~/components/ui/toast";
+import { IconX } from "@tabler/icons-solidjs";
 
 function LogViewer() {
-  const [logViewTabListLeft, setLogViewTabListLeft] = createSignal<string[]>([])
-  const [logViewTabListRight, setLogViewTabListRight] = createSignal<string[]>([])
-  //check drop to other tab
-  const [isDropLeft, setIsDropLeft] = createSignal<boolean>(false)
-  const [isDropRight, setIsDropRight] = createSignal<boolean>(false)
-  const [draggedTabId, setDraggedTabId] = createSignal<[string, string]>()
+  const [logViewTabListLeft, setLogViewTabListLeft] = createSignal<
+    [string, string][]
+  >([]);
+  const [logViewTabListRight, setLogViewTabListRight] = createSignal<
+    [string, string][]
+  >(
+    [],
+  );
 
+  async function openFileDialog(): Promise<[string, string] | undefined> {
+    const path = await open({
+      multiple: false,
+      filters: [
+        { name: "CSV", extensions: ["csv"] },
+      ],
+    });
+
+    if (!path) {
+      return undefined;
+    }
+
+    const extensions = path.slice(path.length - 4, path.length);
+    if (extensions !== ".csv") {
+      return undefined;
+    }
+
+    const tabId = crypto.randomUUID();
+    return [tabId, path.replaceAll("\\", "/")];
+  }
+
+  const [draggedTabIndex, setDraggedTabIndex] = createSignal<number | null>(
+    null,
+  );
+
+  const reorderTabsOnDragOverLeft = (e: DragEvent, index: number) => {
+    e.preventDefault();
+    if (draggedTabIndex() !== null && draggedTabIndex() !== index) {
+      const updateTab = [...logViewTabListLeft()];
+      const [draggedTab] = updateTab.splice(draggedTabIndex()!, 1);
+      updateTab.splice(index, 0, draggedTab);
+      setLogViewTabListLeft(updateTab);
+      setDraggedTabIndex(index);
+    }
+  };
+
+  const reorderTabsOnDragOverRight = (e: DragEvent, index: number) => {
+    e.preventDefault();
+    if (draggedTabIndex() !== null && draggedTabIndex() !== index) {
+      const updateTab = [...logViewTabListRight()];
+      const [draggedTab] = updateTab.splice(draggedTabIndex()!, 1);
+      updateTab.splice(index, 0, draggedTab);
+      setLogViewTabListRight(updateTab);
+      setDraggedTabIndex(index);
+    }
+  };
+
+  //check drop to other tab
+  const [isDropLeft, setIsDropLeft] = createSignal<boolean>(false);
+  const [isDropRight, setIsDropRight] = createSignal<boolean>(false);
+  const [draggedTabId, setDraggedTabId] = createSignal<[string, string]>();
+
+  const toaster = Toast.createToaster({
+    placement: "top-end",
+    gap: 24,
+  });
+
+  const [draggedTabInfo, setDraggedTabInfo] = createSignal<[string, string]>();
 
   return (
     <>
-    <Splitter.Root
-      size={[
-        { id: 'a', size: 50 },
-        { id: 'b', size: 50 },
-      ]}
-      width={"100%"}
-      height={"100%"}
-    >
-      <Splitter.Panel id="a">
-        <div style={{"width" : "100%", "height" : "100%"}}>
-          <LogViewerTabList
-            tabListPosition="left"
-            tabList={logViewTabListLeft()}
-            onTabListChange={(e) => setLogViewTabListLeft(e)}
-            onDraggedTabId={(id) => console.log(id)}
-            onDraggedOut={(e) => {
-              setIsDropLeft(e)
-              console.log(isDropLeft())
-            }}
-          />
-        </div>
-      </Splitter.Panel>
-      <Show when = {logViewTabListLeft().length !== 0}>
-      <Splitter.ResizeTrigger id="a:b" />
-      <Splitter.Panel id="b">
-        <div style={{"width" : "100%", "height" : "100%"}}>
-          <LogViewerTabList
-            tabListPosition="right"
-            tabList={logViewTabListRight()}
-            onTabListChange={(e) => setLogViewTabListRight(e)}
-            onDraggedTabId={(id) => console.log(id)}
-            onDraggedOut={(e) => {
-              setIsDropRight(e)
-              console.log(isDropRight())
-            }}
-          />
-        </div>
-      </Splitter.Panel>
-      </Show>
-    </Splitter.Root>
+      <Toast.Toaster toaster={toaster}>
+        {(toast) => (
+          <Toast.Root>
+            <Toast.Title>{toast().title}</Toast.Title>
+            <Toast.Description>{toast().description}</Toast.Description>
+            <Toast.CloseTrigger>
+              <IconX />
+            </Toast.CloseTrigger>
+          </Toast.Root>
+        )}
+      </Toast.Toaster>
+      <Splitter.Root
+        size={[
+          { id: "a", size: 50 },
+          { id: "b", size: 50 },
+        ]}
+        width={"100%"}
+        height={"100%"}
+      >
+        <Splitter.Panel id="a">
+          <div style={{ "width": "100%", "height": "100%" }}>
+            <LogViewerTabList
+              tabList={logViewTabListLeft()}
+              tabListPosition="left"
+              onCreateTab={async () => {
+                const tabInfo = await openFileDialog();
+                if (!tabInfo) {
+                  toaster.create({
+                    title: "Ivalid File",
+                    description: "The file is invalid.",
+                    type: "error",
+                  });
+                  return;
+                }
+                setLogViewTabListLeft((prev) => [...prev, tabInfo]);
+              }}
+              onDeleteTab={(deleteTabId) =>
+                setLogViewTabListLeft((prev) =>
+                  prev.filter((id) => id[0] !== deleteTabId)
+                )}
+              onReorderTabs={(reorderTabs, index, draggedTabIndex) => {
+                reorderTabsOnDragOverLeft(reorderTabs, index);
+                setDraggedTabIndex(draggedTabIndex);
+              }}
+              onDraggedTabIndex={(index) => {
+                if (!index) setDraggedTabIndex(index);
+              }}
+              onDraggedTabId={(id, path) => {
+                setDraggedTabInfo([id, path]);
+              }}
+              onTabDrop={() => {
+                const checkIsDrop = logViewTabListLeft().filter((prevTabId) =>
+                  prevTabId[0] !== draggedTabInfo()![0]
+                );
+                if (checkIsDrop.length === 0) return;
+                setLogViewTabListRight((prev) => {
+                  return prev.filter((prevTabId) =>
+                    prevTabId[0] !== draggedTabInfo()![0]
+                  );
+                });
+                setLogViewTabListLeft((prev) => {
+                  return [draggedTabInfo()!, ...prev];
+                });
+                setDraggedTabInfo(undefined);
+              }}
+            />
+          </div>
+        </Splitter.Panel>
+        <Show when={logViewTabListLeft().length !== 0}>
+          <Splitter.ResizeTrigger id="a:b" />
+          <Splitter.Panel id="b">
+            <div style={{ "width": "100%", "height": "100%" }}>
+              <div style={{ "width": "100%", "height": "100%" }}>
+                <LogViewerTabList
+                  tabList={logViewTabListRight()}
+                  tabListPosition="right"
+                  onCreateTab={async () => {
+                    const tabInfo = await openFileDialog();
+                    if (!tabInfo) {
+                      toaster.create({
+                        title: "Ivalid File",
+                        description: "The file is invalid.",
+                        type: "error",
+                      });
+                      return;
+                    }
+                    setLogViewTabListRight((prev) => [...prev, tabInfo]);
+                  }}
+                  onDeleteTab={(deletTabid) =>
+                    setLogViewTabListRight((prev) =>
+                      prev.filter((id) => id[0] !== deletTabid)
+                    )}
+                  onReorderTabs={(reorderTabs, index, draggedTabIndex) => {
+                    reorderTabsOnDragOverRight(reorderTabs, index);
+                    setDraggedTabIndex(draggedTabIndex);
+                  }}
+                  onDraggedTabId={(id, path) => {
+                    setDraggedTabInfo([id, path]);
+                  }}
+                  onTabDrop={() => {
+                    const checkIsDrop = logViewTabListRight().filter((
+                      prevTabId,
+                    ) => prevTabId[0] !== draggedTabInfo()![0]);
+                    if (checkIsDrop.length === 0) return;
+                    setLogViewTabListLeft((prev) => {
+                      return prev.filter((prevTabId) =>
+                        prevTabId[0] !== draggedTabInfo()![0]
+                      );
+                    });
+                    setLogViewTabListRight((prev) => {
+                      return [draggedTabInfo()!, ...prev];
+                    });
+                    setDraggedTabInfo(undefined);
+                  }}
+                />
+              </div>
+            </div>
+          </Splitter.Panel>
+        </Show>
+      </Splitter.Root>
     </>
   );
 }
--- src/pages/LogViewer/LogViewerTabList.tsx
+++ src/pages/LogViewer/LogViewerTabList.tsx
@@ -1,62 +1,43 @@
 import { createEffect, createSignal, For, JSX } from "solid-js";
 import { Tabs } from "~/components/ui/tabs";
-import { LogViewerTabPageContent } from "./LogViewerTabPageContent";
 import { IconButton } from "~/components/ui/icon-button";
 import { IconPlus, IconX } from "@tabler/icons-solidjs";
-import { Toast } from "~/components/ui/toast";
-import { FileUploadFileChangeDetails } from "@ark-ui/solid";
 import { Editable } from "~/components/ui/editable";
-import { FileUpload } from "~/components/ui/file-upload";
+import { LogViewerTabPageContent } from "./LogViewerTabPageContent";
 
 export type LogViewerTabListProps = JSX.HTMLAttributes<HTMLDivElement> & {
-    tabList? : string[],
-    tabListPosition : string,
-    onTabListChange? : (tabList: string[]) => void;
-    onDraggedOut? : (isDraggedOut :  boolean) => void;
-    onDraggedTabId? : (tabId: [string, string]) => void;
-}
+  tabList: [string, string][];
+  tabListPosition: string;
+  onCreateTab?: () => void;
+  onDeleteTab?: (tadId: string) => void;
+  onDraggedTabIndex?: (index: number | null) => void;
+  onReorderTabs?: (
+    e: DragEvent,
+    tabIndex: number,
+    draggedTabIndex: number | null,
+  ) => void;
+  onDraggedTabId?: (tabId: string, filePath: string) => void;
+  onTabDrop?: () => void;
+};
 
-export function LogViewerTabList(props : LogViewerTabListProps) {
+export function LogViewerTabList(props: LogViewerTabListProps) {
   // Reorder tab
   const [draggedTabIndex, setDraggedTabIndex] = createSignal<number | null>(
     null,
   );
-  // The signal can know when tab delete button has pressed
-  const [isDeleteButtonPressed, setIsDeleteButtonPressed] = createSignal<
-    boolean
-  >(
-    false,
-  );
 
-  // Reorder tab by dragging
-  const reorderTabsOnDragOver = (e: DragEvent, index: number) => {
-    e.preventDefault();
-    if (draggedTabIndex() !== null && draggedTabIndex() !== index) {
-      const updateTab = [...tabIdList()];
-      const [draggedTab] = updateTab.splice(draggedTabIndex()!, 1);
-      updateTab.splice(index, 0, draggedTab);
-      setTabIdList(updateTab);
-      setDraggedTabIndex(index);
-    }
-  };
-
-  // Drag Scroll this need to be deleted
-  const [isDragScrolling, setIsDragScrolling] = createSignal(false);
-  // Mouse X coordinate on screen.
   const [clientX, setClientX] = createSignal<number | null>(null);
   // Tab X coordinate relative to start of tab list, can extend beyond screen.
   const [prevPositionX, setPrevPositionX] = createSignal<number>(0);
   let scrollContainer: HTMLDivElement | undefined;
 
-  // Handles scrolling tab list during drag/reorder.
   const dragOverScroll = (e: MouseEvent) => {
-    if (!isDragScrolling() || !scrollContainer || !clientX()) return;
+    if (!scrollContainer || !clientX()) return;
 
     const movement = (e.clientX - clientX()! + prevPositionX()) * 0.05;
     scrollContainer.scrollBy({ left: movement });
   };
 
-  // Scrolls tab list to the end.
   const scrollToEnd = () => {
     if (!scrollContainer) return;
     if (scrollContainer.offsetWidth !== scrollContainer.scrollWidth) {
@@ -65,207 +46,146 @@ export function LogViewerTabList(props : LogViewerTabListProps) {
   };
 
   const mouseWheelHandler = (e: WheelEvent) => {
-    if (!scrollContainer || isDragScrolling()) return;
+    if (!scrollContainer) return;
 
     e.preventDefault;
     scrollContainer.scrollLeft += e.deltaY;
   };
 
-  // Active tab ID
   const [focusedTab, setFocusedTab] = createSignal<string>("");
-  const [tabIdList, setTabIdList] = createSignal([] as string[]);
-  if(props.tabList) setTabIdList(props.tabList)
+  const [isTabDeleted, setIsTabDeleted] = createSignal<boolean>(false);
+  const [deleteTabIndex, setDeleteTabIndex] = createSignal<number | null>(null);
 
-  function deleteTab(index: number) {
-    if (isEditMode()) return;
-    const contextIndex = index === 0 ? 1 : index - 1;
-    const newValue = focusedTab() === tabIdList()[index]
-      ? tabIdList()[contextIndex]
-      : focusedTab();
-
-    setTimeout(() => {
-      setFocusedTab(newValue);
-    }, 0);
-    setTabIdList((prev) => {
-      const updateTabCtx = prev.filter((_, i) => i !== index);
-      return updateTabCtx;
-    });
-  }
-
-  // Every tablist value change send changed tab list data to parent component
+  // Focus tab whenether it's deleted or created
   createEffect(() => {
-    const tabList = tabIdList()
-    props.onTabListChange?.(tabList)
-  })
+    const tabIdList = props.tabList;
+    if (tabIdList.length === 0) return;
 
-  let file: FileUploadFileChangeDetails;
-
-  const toaster = Toast.createToaster({
-    placement: "top-end",
-    gap: 24,
+    if (isTabDeleted()) {
+      if (!deleteTabIndex) return;
+      const contextIndex = deleteTabIndex() === 0 ? 1 : deleteTabIndex()! - 1;
+      const newValue = focusedTab() === props.tabList[deleteTabIndex()!][0]
+        ? props.tabList[contextIndex][0]
+        : focusedTab();
+      setTimeout(() => {
+        setFocusedTab(newValue);
+        setDeleteTabIndex(null);
+      }, 0);
+    } else {
+      const currentTabId = props.tabList[props.tabList.length - 1][0];
+      setFocusedTab(currentTabId);
+      scrollToEnd();
+    }
   });
 
-  // Check if the tab is editable and in edit mode to disable the delete button.
-  const [isEditMode, setIsEditMode] = createSignal<boolean>(false);
-
-  //const [dropIndex, setDropIndex] = createSignal<number | null>(0)
+  const [draggedTabInfo, setDraggedTabInfo] = createSignal<[string, string]>();
 
   return (
     <>
-      <Toast.Toaster toaster={toaster}>
-        {(toast) => (
-          <Toast.Root>
-            <Toast.Title>{toast().title}</Toast.Title>
-            <Toast.Description>{toast().description}</Toast.Description>
-            <Toast.CloseTrigger>
-              <IconX />
-            </Toast.CloseTrigger>
-          </Toast.Root>
-        )}
-      </Toast.Toaster>
-      <Tabs.Root
-        value={focusedTab()}
-        onValueChange={(e) => setFocusedTab(e.value)}
-        width="100%"
-        height="100%"
-      >
-        <Tabs.List
-          ref={scrollContainer}
-          style={{
-            background: "--colors-bg-muted",
-            height: "3rem",
-            "padding-top": "0.5rem",
-          }}
-          gap="0"
-          onWheel={(e) => mouseWheelHandler(e)}
-          onDrop={(e) =>{ 
-            const tabId = tabIdList()[draggedTabIndex()!]
-            if (tabIdList().indexOf(tabId) !== -1){
-              props.onDraggedOut?.(false)
-              return;
-            } 
-            props.onDraggedOut?.(true)
-          }
-          }
+      <div style={{ "width": "100%", height: "100%" }}>
+        <Tabs.Root
+          value={focusedTab()}
+          onValueChange={(e) => setFocusedTab(e.value)}
+          width="100%"
+          height="100%"
         >
-          <For each={tabIdList()}>
-            {(tabId, index) => (
-              <>
+          <Tabs.List
+            ref={scrollContainer}
+            style={{
+              background: "--colors-bg-muted",
+              height: `3rem`,
+              "padding-top": "0.5rem",
+            }}
+            gap="0"
+            onWheel={(e) => mouseWheelHandler(e)}
+            onDrop={() => {
+              props.onTabDrop?.();
+            }}
+            width={"100%"}
+          >
+            <For each={props.tabList}>
+              {([tabId, filePath], index) => (
                 <Tabs.Trigger
                   value={tabId}
                   draggable
                   onDragStart={(e) => {
-                    if (isDeleteButtonPressed()!) {
-                      setIsDeleteButtonPressed(false);
-                      return;
-                    }
                     setDraggedTabIndex(index());
                     setFocusedTab(tabId);
-                    props.onDraggedTabId?.([tabId, props.tabListPosition]);
                     setPrevPositionX(e.currentTarget.scrollLeft);
                     setClientX(e.clientX);
-                    setIsDragScrolling(true);
+
+                    //drag to other tab
+                    props.onDraggedTabId?.(tabId, filePath);
+                    setDraggedTabInfo([tabId, filePath]);
                   }}
                   onDragOver={(e) => {
-                    reorderTabsOnDragOver(e, index());
+                    props.onReorderTabs?.(
+                      e,
+                      Number(index()),
+                      draggedTabIndex(),
+                    );
                     dragOverScroll(e);
                   }}
                   onDragEnd={() => {
-                    setClientX(null);
-                    setIsDragScrolling(false);
                     setDraggedTabIndex(null);
+                    setClientX(null);
                   }}
-                  style={{ "padding-right": "0", "padding-top": "0.1rem" }}
-                  height={"100%"}
                 >
                   <Editable.Root
-                    defaultValue={file.acceptedFiles[0].name /*tab name*/}
+                    defaultValue={
+                      JSON.stringify(filePath.match((/[^?!//]+$/)!))
+                        .slice(2, -2) /*change to tabname*/
+                    }
                     activationMode="dblclick"
-                    onEditChange={() => setIsEditMode(!isEditMode())}
+                    onEditChange={() => {}}
                   >
                     <Editable.Area>
-                      <Editable.Input />
-                      <Editable.Preview />
+                      <Editable.Input width="100%" />
+                      <Editable.Preview width="100%" />
                     </Editable.Area>
                   </Editable.Root>
                   <IconButton
+                    variant={"ghost"}
+                    size={"sm"}
                     onClick={() => {
-                      deleteTab(index());
+                      setDeleteTabIndex(index());
+                      props.onDeleteTab?.(tabId);
+                      setIsTabDeleted(true);
                     }}
-                    variant="ghost"
-                    size="xs"
-                    width="1rem"
-                    borderRadius="1rem"
-                    draggable
-                    cursor={isDragScrolling() ? "not-allowed" : "auto"}
-                    onDragStart={(e) => {
-                      e.dataTransfer?.setDragImage(
-                        document.createElement("img"),
-                        0,
-                        0,
-                      );
-                      setIsDeleteButtonPressed(true);
-                    }}
-                    onDragOver={() => {
-                      return;
-                    }}
-                    disabled={isEditMode()}
                   >
                     <IconX />
                   </IconButton>
                 </Tabs.Trigger>
-              </>
+              )}
+            </For>
+            <IconButton
+              variant={"ghost"}
+              onClick={() => {
+                setIsTabDeleted(false);
+                props.onCreateTab?.();
+              }}
+              borderRadius={"1rem"}
+              paddingBottom={"0.5rem"}
+            >
+              <IconPlus />
+            </IconButton>
+            <Tabs.Indicator />
+          </Tabs.List>
+          <For each={props.tabList}>
+            {([currentTabId, currentFilePath]) => (
+              <Tabs.Content
+                value={currentTabId}
+                height={"100%"}
+              >
+                <LogViewerTabPageContent
+                  tabId={currentTabId}
+                  filePath={currentFilePath}
+                />
+              </Tabs.Content>
             )}
           </For>
-          <FileUpload.Root
-            accept="text/csv"
-            minFileSize={3}
-            onFileChange={(details) => {
-              file = details;
-              const tabId = crypto.randomUUID();
-              setTabIdList((prev) => {
-                return [...prev, tabId];
-              });
-              setFocusedTab(tabId);
-              scrollToEnd();
-            }}
-          >
-            <FileUpload.Trigger
-              asChild={(triggerProps) => (
-                <IconButton
-                  size={"xs"}
-                  variant={"ghost"}
-                  {...triggerProps()}
-                  width={"1rem"}
-                  borderRadius={"1rem"}
-                >
-                  <IconPlus />
-                </IconButton>
-              )}
-            />
-            <FileUpload.HiddenInput />
-          </FileUpload.Root>
-          <Tabs.Indicator />
-        </Tabs.List>
-        <For each={tabIdList()}>
-          {(tabId, index) => (
-            <Tabs.Content
-              value={tabId}
-              height={"100%"}
-              style={{ "overflow-y": "auto" }}
-            >
-              <LogViewerTabPageContent
-                tabId={tabId}
-                details={file}
-                onErrorMessage={(msg) => {
-                  toaster.create(msg);
-                  deleteTab(index());
-                }}
-              />
-            </Tabs.Content>
-          )}
-        </For>
-      </Tabs.Root>
+        </Tabs.Root>
+      </div>
     </>
   );
 }
