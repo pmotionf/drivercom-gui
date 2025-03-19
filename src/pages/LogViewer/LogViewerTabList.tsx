cd2aae06d1589b99dcdefa58625028142c939321
kmtso434 <kmtso434@naver.com>
2025-03-19 15:26:02 +0900

    fix: Linux reorder tab error

--- src/pages/LogViewer/LogViewerTabList.tsx
+++ src/pages/LogViewer/LogViewerTabList.tsx
@@ -5,16 +5,21 @@ import { IconPlus, IconX } from "@tabler/icons-solidjs";
 import { Editable } from "~/components/ui/editable";
 import { LogViewerTabPageContent } from "./LogViewerTabPageContent";
 import { LogViewerTabContext } from "../LogViewer";
-import { css } from "styled-system/css";
+import {
+  closestCenter,
+  createSortable,
+  DragDropProvider,
+  DragDropSensors,
+  SortableProvider,
+} from "@thisbeyond/solid-dnd";
+import { Stack } from "styled-system/jsx";
 
 export type LogViewerTabListProps = JSX.HTMLAttributes<HTMLDivElement> & {
   id: string;
   tabList: LogViewerTabContext[];
   onCreateTab?: () => void;
   onDeleteTab?: (deleteTabId: string, index: number) => void;
-  onDraggedTabInfo?: (
-    tabContext: LogViewerTabContext,
-  ) => void;
+  onDraggedTabInfo?: (tabContext: LogViewerTabContext) => void;
   onTabDrop?: () => void;
   onTabContextChange?: (tabContext: LogViewerTabContext) => void;
   onTabContextDrag?: (isTabContextDragEnter: boolean) => void;
@@ -24,46 +29,7 @@ export type LogViewerTabListProps = JSX.HTMLAttributes<HTMLDivElement> & {
 };
 
 export function LogViewerTabList(props: LogViewerTabListProps) {
-  // Reorder tab
-  const [draggedTabIndex, setDraggedTabIndex] = createSignal<number | null>(
-    null,
-  );
-
-  const reorderTabsOnDragOver = (e: DragEvent, index: number) => {
-    e.preventDefault();
-    if (draggedTabIndex() !== null && draggedTabIndex() !== index) {
-      const updateTab = [...props.tabList];
-      const [draggedTab] = updateTab.splice(draggedTabIndex()!, 1);
-      updateTab.splice(index, 0, draggedTab);
-      props.onTabReorder?.([...updateTab]);
-      setDraggedTabIndex(index);
-    }
-  };
-
-  // Keep update the draggedTabIndex to reorder tab until the tab drag ends.
-  createEffect(() => {
-    const index = draggedTabIndex();
-    if (index === null) {
-      const newIndex = props.tabList.findIndex((tab) =>
-        tab.id === props.focusedTab
-      );
-      if (newIndex !== -1) {
-        setDraggedTabIndex(newIndex);
-      }
-    }
-  });
-
-  const [clientX, setClientX] = createSignal<number | null>(null);
-  // Tab X coordinate relative to start of tab list, can extend beyond screen.
-  const [prevPositionX, setPrevPositionX] = createSignal<number>(0);
   let scrollContainer: HTMLDivElement | undefined;
-
-  const dragOverScroll = (e: MouseEvent) => {
-    if (!scrollContainer || !clientX()) return;
-
-    const movement = (e.clientX - clientX()! + prevPositionX()) * 0.05;
-    scrollContainer.scrollBy({ left: movement });
-  };
 
   const mouseWheelHandler = (e: WheelEvent) => {
     if (!scrollContainer) return;
@@ -83,9 +49,7 @@ export function LogViewerTabList(props: LogViewerTabListProps) {
 
   return (
     <>
-      <div
-        style={{ "width": "100%", height: "100%" }}
-      >
+      <div style={{ width: "100%", height: "100%" }}>
         <Tabs.Root
           value={props.focusedTab!}
           onValueChange={(e) => {
@@ -110,71 +74,35 @@ export function LogViewerTabList(props: LogViewerTabListProps) {
             width="100%"
             marginRight="0"
           >
-            <For each={props.tabList}>
-              {(tab, index) => (
-                <Tabs.Trigger
-                  id={tab.id}
-                  value={tab.id}
-                  draggable
-                  paddingRight="0"
-                  onDragStart={(e) => {
-                    setDraggedTabIndex(index());
-                    setPrevPositionX(e.currentTarget.scrollLeft);
-                    setClientX(e.clientX);
-
-                    const changedTabContext =
-                      props.tabList.filter((info) => info.id === tab.id)[0];
-                    props.onDraggedTabInfo?.(changedTabContext);
-                  }}
-                  onDragOver={(e) => {
-                    reorderTabsOnDragOver(e, index());
-                    dragOverScroll(e);
-                  }}
-                  onDragEnd={() => {
-                    setDraggedTabIndex(null);
-                    setClientX(null);
-                  }}
-                  // Use css for theme color
-                  class={css({
-                    "borderBottomWidth": props.focusedTab === tab.id
-                      ? "3px"
-                      : "0px",
-                    "marginTop": props.focusedTab === tab.id
-                      ? `calc(0.5rem + 1px)`
-                      : `0.5rem `,
-                    "borderBottomColor": "accent.emphasized",
-                  })}
-                >
-                  <Editable.Root
-                    defaultValue={tab.tabName.length === 0
-                      ? JSON.stringify(tab.filePath.match((/[^?!//]+$/)!))
-                        .slice(2, -2) /*change to tabname*/
-                      : tab.tabName}
-                    activationMode="dblclick"
-                    onValueCommit={(tabName) => {
-                      const tabUpdate = tab;
-                      tabUpdate.tabName = tabName.value;
-                      props.onTabContextChange?.(tabUpdate);
-                    }}
-                  >
-                    <Editable.Area>
-                      <Editable.Input width="100%" />
-                      <Editable.Preview width="100%" />
-                    </Editable.Area>
-                  </Editable.Root>
-                  <IconButton
-                    variant="ghost"
-                    size="sm"
-                    onClick={() => {
-                      props.onDeleteTab?.(tab.id, index());
-                    }}
-                    borderRadius="3rem"
-                  >
-                    <IconX />
-                  </IconButton>
-                </Tabs.Trigger>
-              )}
-            </For>
+            <DraggableTabList
+              tabIds={props.tabList.map((tab) => tab.id)}
+              tabNames={props.tabList.map((tab) => tab.tabName)}
+              filePath={props.tabList.map((tab) => tab.filePath)}
+              focusedId={props.focusedTab!}
+              onTabNameChange={(changedTabName, changedTabId) => {
+                const tabUpdate = props.tabList[
+                  props.tabList.map((tab) => tab.id).indexOf(changedTabId)
+                ];
+                tabUpdate.tabName = changedTabName;
+                props.onTabContextChange?.(tabUpdate);
+              }}
+              onDeleteTab={(deleteTabId, deleteTabIndex) => {
+                props.onDeleteTab?.(deleteTabId, deleteTabIndex);
+              }}
+              onReorderTab={(reorderedTabList) => {
+                const reorderedTabIndex: number[] = reorderedTabList.map(
+                  (reorderTabId) => {
+                    return props.tabList
+                      .map((prevTab) => prevTab.id)
+                      .indexOf(reorderTabId);
+                  },
+                );
+                const updatedTab = props.tabList.map((_, i) => {
+                  return props.tabList[reorderedTabIndex[i]];
+                });
+                props.onTabReorder?.(updatedTab);
+              }}
+            />
             <IconButton
               variant="ghost"
               onClick={() => props.onCreateTab?.()}
@@ -183,11 +111,6 @@ export function LogViewerTabList(props: LogViewerTabListProps) {
             >
               <IconPlus />
             </IconButton>
-            <div
-              style={{ "width": "100%" }}
-              onDragOver={(e) => e.preventDefault()}
-            >
-            </div>
           </Tabs.List>
 
           <For each={props.tabList}>
@@ -240,3 +163,133 @@ export function LogViewerTabList(props: LogViewerTabListProps) {
     </>
   );
 }
+
+export type DraggbleTabTriggerProps = {
+  item: string;
+  tabName: string;
+  focus: boolean;
+  onTabNameChange?: (changedTabName: string) => void;
+  onDeleteButtonClick?: (deleteTabId: string) => void;
+};
+
+const DraggableTabTrigger = (props: DraggbleTabTriggerProps) => {
+  const sortable = createSortable(props.item);
+  const editableAreaWidth = "10rem";
+
+  return (
+    <div
+      //@ts-ignore: Necessary to use `use:sortable`
+      use:sortable
+      class="sortable"
+      classList={{ "opacity-25": sortable.isActiveDraggable }}
+      style={{ "padding-top": "0.5rem", width: "100%" }}
+    >
+      <Tabs.Trigger
+        id={props.item}
+        value={props.item}
+        borderBottomWidth={props.focus ? "2px" : "0px"}
+        borderBottomColor="accent.10"
+      >
+        <Editable.Root
+          id="editable_component"
+          defaultValue={props.tabName}
+          activationMode="dblclick"
+          onValueCommit={(tabName) => {
+            props.onTabNameChange?.(tabName.value);
+          }}
+        >
+          <Editable.Area>
+            <Editable.Input width={editableAreaWidth} />
+            <Editable.Preview width={editableAreaWidth} />
+          </Editable.Area>
+        </Editable.Root>
+        <IconButton
+          variant="ghost"
+          size="sm"
+          onClick={() => {
+            props.onDeleteButtonClick?.(props.item);
+          }}
+          borderRadius="3rem"
+        >
+          <IconX />
+        </IconButton>
+      </Tabs.Trigger>
+    </div>
+  );
+};
+
+export type drggableTabListProps = {
+  tabIds: string[];
+  tabNames: string[];
+  filePath: string[];
+  focusedId: string;
+  onDragFocusedId?: (tabId: string) => void;
+  onTabNameChange?: (changedTabName: string, changedTabId: string) => void;
+  onDeleteTab?: (deletedTabId: string, deleteTabIndex: number) => void;
+  onReorderTab?: (tabIds: string[]) => void;
+};
+
+export const DraggableTabList = (props: drggableTabListProps) => {
+  const [items, setItems] = createSignal<string[]>([...props.tabIds]);
+  const [activeItem, setActiveItem] = createSignal(null);
+  const ids = () => items();
+
+  //@ts-ignore: ts-ignore is necessary to use onDragStart event.
+  const onDragStart = ({ draggable }) => setActiveItem(draggable.id);
+
+  //@ts-ignore: ts-ignore is necessary to use onDragEnd event.
+  const onDragEnd = ({ draggable, droppable }) => {
+    if (draggable && droppable) {
+      const currentItems = ids();
+      const fromIndex = currentItems.indexOf(draggable.id);
+      const toIndex = currentItems.indexOf(droppable.id);
+      if (fromIndex !== toIndex) {
+        const updatedItems = currentItems.slice();
+        updatedItems.splice(toIndex, 0, ...updatedItems.splice(fromIndex, 1));
+        setItems(updatedItems);
+        props.onReorderTab?.(items());
+      }
+    }
+    setActiveItem(null);
+  };
+
+  createEffect(() => {
+    if (activeItem() === null) return;
+    props.onDragFocusedId?.(activeItem()!);
+  });
+
+  return (
+    <DragDropProvider
+      onDragStart={onDragStart}
+      //@ts-ignore: For testing solid js dnd
+      onDragEnd={onDragEnd}
+      collisionDetector={closestCenter}
+    >
+      <DragDropSensors />
+      <Stack direction="row">
+        <SortableProvider ids={ids()}>
+          <For each={items()}>
+            {(item, index) => (
+              <DraggableTabTrigger
+                item={item}
+                tabName={props.tabNames[index()].length === 0
+                  ? JSON.stringify(
+                    props.filePath[index()].match(/[^?!//]+$/!),
+                  ).slice(2, -2) /*change to tabname*/
+                  : props.tabNames[index()]}
+                focus={item === props.focusedId}
+                onTabNameChange={(changedTabName) => {
+                  props.onTabNameChange?.(changedTabName, item);
+                }}
+                onDeleteButtonClick={(deleteTabId) => {
+                  const deletedTabIndex = props.tabIds.indexOf(deleteTabId);
+                  props.onDeleteTab?.(deleteTabId, deletedTabIndex);
+                }}
+              />
+            )}
+          </For>
+        </SortableProvider>
+      </Stack>
+    </DragDropProvider>
+  );
+};
