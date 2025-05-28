5fb8ec57b15f1d452674e42f002c5ab39218f5b0
kmtso434 <kmtso434@naver.com>
2025-05-28 15:58:50 +0900

    feat: Add side bar for connect server

--- src/pages/Monitoring.tsx
+++ src/pages/Monitoring.tsx
@@ -1,35 +1,138 @@
-import { createEffect, onMount } from "solid-js";
+import { onMount } from "solid-js";
 import { Button } from "~/components/ui/styled/button.tsx";
-import net from "node:net";
+import { connect, listen } from "@kuyoonjo/tauri-plugin-tcp";
+import { Stack } from "styled-system/jsx";
+import { Text } from "~/components/ui/text";
 
 function Monitoring() {
-  onMount(() => {
-    const client = new net.Socket();
+  onMount(async () => {
+    const client = crypto.randomUUID();
     const host = "192.168.0.7";
     const port = 9001;
 
-    client.connect(port, host, () => {
-      console.log("test");
-      client.write("Hello, server! Love, Client.");
+    await connect(client, `${host}:${port}`);
+    await listen((x) => {
+      console.log(x.payload!.event!.message!.data);
     });
   });
 
-  /*load("all.proto", function (err, root) {
-    if (err) {
-      console.log(err);
-      throw err;
-    }
-
-    const connect = root?.lookupType("SendCommand.GetX");
-    let message = connect?.create({ line_idx: "left", axis_idx: 1 });
-    let buffer = connect?.encode(message!).finish();
-    let decoded = connect?.decode(buffer!);
-    });*/
-
   return (
-    <>
-      <Button>test</Button>
-    </>
+    <Stack direction="row" width="100%" height="100%">
+      <div style={{ height: "100%", width: `calc(100% - 20rem)` }}></div>
+      <Stack
+        style={{
+          height: "100%",
+          width: "20rem",
+          position: "fixed",
+          left: `calc(100% - 20rem)`,
+        }}
+        borderLeftWidth="2px"
+        gap="0"
+      >
+        <Stack width="100%" borderBottomWidth="2px" padding="1rem">
+          {/* Line Area */}
+          <Text fontWeight="bold" size="lg">
+            Line
+          </Text>
+          <Stack direction="row" width="100%">
+            <Text
+              size="sm"
+              width="20%"
+              color="fg.muted"
+              marginRight="0.5rem"
+              marginTop="0.4rem"
+            >
+              ID
+            </Text>
+            <Stack
+              background="bg.muted"
+              width="80%"
+              borderRadius="0.5rem"
+              height="2rem"
+            >
+              <input
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  border: "none",
+                  outline: "none",
+                  "white-space": "nowrap",
+                  overflow: "hidden",
+                  display: "block",
+                  "text-overflow": "ellipsis",
+                  "padding-left": "0.5rem",
+                }}
+              />
+            </Stack>
+          </Stack>
+        </Stack>
+        {/* Connect Area */}
+        <Stack padding="1rem" width="100%" borderBottomWidth="2px">
+          <Text size="lg" fontWeight="bold">
+            Connect
+          </Text>
+          <Stack direction="row" width="100%">
+            <Stack width="50%">
+              <Text size="sm" width="100%" color="fg.muted" marginTop="0.4rem">
+                IP
+              </Text>
+              <Stack
+                background="bg.muted"
+                width="100%"
+                borderRadius="0.5rem"
+                height="2rem"
+              >
+                <input
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    border: "none",
+                    outline: "none",
+                    "white-space": "nowrap",
+                    overflow: "hidden",
+                    display: "block",
+                    "text-overflow": "ellipsis",
+                    "padding-left": "0.5rem",
+                  }}
+                />
+              </Stack>
+            </Stack>
+            <Stack width="50%">
+              <Text
+                size="sm"
+                width="100%"
+                color="fg.muted"
+                marginRight="0.5rem"
+                marginTop="0.4rem"
+              >
+                Port
+              </Text>
+              <Stack
+                background="bg.muted"
+                width="100%"
+                borderRadius="0.5rem"
+                height="2rem"
+              >
+                <input
+                  style={{
+                    width: "100%",
+                    height: "100%",
+                    border: "none",
+                    outline: "none",
+                    "white-space": "nowrap",
+                    overflow: "hidden",
+                    display: "block",
+                    "text-overflow": "ellipsis",
+                    "padding-left": "0.5rem",
+                  }}
+                />
+              </Stack>
+            </Stack>
+          </Stack>
+          <Button>Connect</Button>
+        </Stack>
+      </Stack>
+    </Stack>
   );
 }
 
