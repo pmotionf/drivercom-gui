c2c0cbd1df8d629dcf88999e79350b3f456ebe85
kmtso434 <kmtso434@naver.com>
2025-06-26 14:55:27 +0900

    fix: Remove connect page

--- src/index.tsx
+++ src/index.tsx
@@ -7,7 +7,6 @@ import { Route, Router } from "@solidjs/router";
 import App from "./App";
 const Configuration = lazy(() => import("./pages/Configuration"));
 const LogViewer = lazy(() => import("./pages/LogViewer"));
-const Connect = lazy(() => import("./pages/Connect"));
 const Logging = lazy(() => import("./pages/Logging"));
 
 render(
@@ -15,7 +14,6 @@ render(
     <Router root={App}>
       <Route path="/configuration" component={Configuration} />
       <Route path="/logviewer" component={LogViewer} />
-      <Route path="/connect" component={Connect} />
       <Route path="/logging" component={Logging} />
     </Router>
   ),
