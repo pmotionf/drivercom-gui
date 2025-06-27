fce42e681cce56461cfc8fd169981d798a53ba2b
kmtso434 <kmtso434@naver.com>
2025-06-27 10:54:36 +0900

    feat: Show system status in UI

---
+++ src/components/System/Axes.tsx
@@ -0,0 +1,103 @@
+import { Stack } from "styled-system/jsx";
+import { Text } from "../ui/text.tsx";
+import { Show } from "solid-js/web";
+import { AxesContext } from "./Station.tsx";
+import { useContext } from "solid-js";
+import { Badge } from "../ui/badge.tsx";
+
+export type AxesInfo = {
+  hallAlarm?: { front?: boolean; back?: boolean };
+  motorEnabled?: boolean;
+  carrierId?: number;
+  waitingPull?: boolean;
+  waitingPush?: boolean;
+  overCurrent?: boolean;
+};
+
+export function Axis() {
+  const axesContext: { axes: AxesInfo; id: string } | undefined = useContext(
+    AxesContext,
+  );
+  if (!axesContext) return;
+
+  return (
+    <Stack
+      width="9rem"
+      height="9rem"
+      borderRadius="0.5rem"
+      borderWidth="1px"
+      borderRightWidth={axesContext.axes.hallAlarm &&
+          axesContext.axes.hallAlarm.front
+        ? "5px"
+        : "1px"}
+      borderLeftWidth={axesContext.axes.hallAlarm &&
+          axesContext.axes.hallAlarm.back
+        ? "5px"
+        : "1px"}
+      borderRightColor={axesContext.axes.hallAlarm &&
+          axesContext.axes.hallAlarm.front
+        ? "accent.customGreen"
+        : undefined}
+      borderLeftColor={axesContext.axes.hallAlarm &&
+          axesContext.axes.hallAlarm.back
+        ? "accent.customGreen"
+        : undefined}
+      backgroundColor="bg.default"
+      padding="0.5rem"
+      gap="0"
+    >
+      <Stack
+        height="min-content"
+        width="100%"
+        borderBottomWidth="0.02px"
+        gap="1"
+        direction="column"
+        paddingBottom="0.3rem"
+        borderColor="bg.muted"
+      >
+        <Badge
+          width="min-content"
+          backgroundColor={axesContext.axes.overCurrent
+            ? "red"
+            : axesContext.axes.motorEnabled
+            ? "accent.customGreen"
+            : "bg.emphasized"}
+          paddingLeft="0.5rem"
+          paddingRight="0.5rem"
+          height="min-content"
+          borderWidth="0"
+        >
+          <Text
+            color={axesContext.axes.overCurrent ? "#ffffff" : "fg.default"}
+            size="sm"
+            fontWeight="medium"
+          >
+            Axis {axesContext.id.split(":")[1]}
+          </Text>
+        </Badge>
+      </Stack>
+      <Show when={axesContext.axes.carrierId}>
+        <Text
+          fontWeight="bold"
+          height="1rem"
+          width="100%"
+          color="fg.default"
+          marginBottom="0.5rem"
+        >
+          Carrier {axesContext.axes.carrierId}
+        </Text>
+      </Show>
+
+      <Show when={axesContext.axes.waitingPush}>
+        <Text width="100%" size="sm">
+          Waiting push
+        </Text>
+      </Show>
+      <Show when={axesContext.axes.waitingPull}>
+        <Text width="100%" size="sm">
+          Waiting pull
+        </Text>
+      </Show>
+    </Stack>
+  );
+}
