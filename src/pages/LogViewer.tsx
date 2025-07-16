61135c1d62c0528ecad25c14a0ce7e41743ce56e
kmtso434 <kmtso434@naver.com>
2025-07-16 16:28:21 +0900

    fix: Log get toast msg

--- src/pages/LogViewer.tsx
+++ src/pages/LogViewer.tsx
@@ -1,9 +1,10 @@
 import { PanelLayout } from "~/components/PanelLayout.tsx";
+import { Pages } from "~/GlobalState";
 
 function LogViewer() {
   return (
     <div style={{ "overflow-y": "hidden", width: `100%`, height: "100%" }}>
-      <PanelLayout id="LogViewer" />
+      <PanelLayout id={Pages.LogViewer} />
     </div>
   );
 }
