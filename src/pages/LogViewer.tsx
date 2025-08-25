7c0c1d208ced94b8ea9a4d12082b982a8f70d4ac
kmtso434 <kmtso434@naver.com>
2025-08-25 13:40:21 +0900

    fix: Apply file fn in Log viewer page

--- src/pages/LogViewer.tsx
+++ src/pages/LogViewer.tsx
@@ -3,37 +3,37 @@ import { PanelLayout } from "~/components/PanelLayout.tsx";
 import { TabContext, TabList } from "~/components/TabList";
 import { Pages, tabContexts } from "~/GlobalState";
 import { LogViewerTabPageContent } from "./LogViewer/LogViewerTabPageContent";
-import { open } from "@tauri-apps/plugin-dialog";
 import { Toast } from "~/components/ui/toast";
 import { IconX } from "@tabler/icons-solidjs";
+import { file } from "~/utils/file";
 
 function LogViewer() {
-  async function openFileDialog(): Promise<{
-    id: string;
-    filePath: string;
-  } | null> {
-    const path = await open({
-      multiple: false,
-      filters: [{ name: "CSV", extensions: ["csv"] }],
-    });
-
-    if (!path) {
-      return null;
-    }
-
-    const extensions = path.slice(path.length - 4, path.length);
-    if (extensions !== ".csv") {
-      return null;
-    }
-
-    const tabId = crypto.randomUUID();
-    return { id: tabId, filePath: path.replaceAll("\\", "/") };
-  }
-
   const toaster = Toast.createToaster({
     placement: "top-end",
     gap: 24,
   });
+
+  const newTabCtx = (filePath: string): TabContext => {
+    return {
+      tab: {
+        id: crypto.randomUUID(),
+        tabName: filePath
+          .replaceAll("\\", "/")
+          .match(/[^?!//]+$/!)!
+          .toString()
+          .slice(0, -4) as string,
+      },
+      tabPage: {
+        logViewerTabPage: {
+          filePath: filePath.replaceAll("\\", "/"),
+          plotSplitIndex: [],
+          plotContext: [],
+          plotXScale: [0, 0],
+        },
+        configTabPage: null,
+      },
+    };
+  };
 
   return (
     <div style={{ "overflow-y": "hidden", width: `100%`, height: "100%" }}>
@@ -41,42 +41,24 @@ function LogViewer() {
         <Panel>
           <TabList
             onCreateTab={async (key) => {
-              const newTabInfo = await openFileDialog();
-              if (!newTabInfo) {
-                toaster.create({
-                  title: "Invalid File",
-                  description: "The file is invalid.",
-                  type: "error",
-                });
-                return;
-              }
-
-              const newTab: TabContext = {
-                tab: {
-                  id: newTabInfo.id,
-                  tabName: newTabInfo.filePath
-                    .replaceAll("\\", "/")
-                    .match(/[^?!//]+$/!)!
-                    .toString()
-                    .slice(0, -4) as string,
-                },
-                tabPage: {
-                  logViewerTabPage: {
-                    filePath: newTabInfo.filePath.replaceAll("\\", "/"),
-                    plotSplitIndex: [],
-                    plotContext: [],
-                    plotXScale: [0, 0],
-                  },
-                  configTabPage: null,
-                },
-              };
-
-              if (tabContexts.has(key)) {
-                const tabCtx = tabContexts.get(key)!;
-                tabCtx[1]({
-                  tabContext: [...tabCtx[0].tabContext, newTab],
-                  focusedTab: newTab.tab.id,
-                });
+              try {
+                const filePath = await file.openDialog("csv");
+                const newTab = newTabCtx(filePath);
+                if (tabContexts.has(key)) {
+                  const tabCtx = tabContexts.get(key)!;
+                  tabCtx[1]({
+                    tabContext: [...tabCtx[0].tabContext, newTab],
+                    focusedTab: newTab.tab.id,
+                  });
+                }
+              } catch (e) {
+                if (e) {
+                  toaster.create({
+                    title: "Invalid File",
+                    description: e.toString(),
+                    type: "error",
+                  });
+                }
               }
             }}
           >
