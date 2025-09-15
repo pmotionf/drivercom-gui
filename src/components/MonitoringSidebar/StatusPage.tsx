e2ab4710afca0079406ca9380a793f7f01229149
kmtso434 <kmtso434@naver.com>
2025-09-15 16:05:18 +0900

    feat: Put Error table in tab page

---
+++ src/components/MonitoringSidebar/StatusPage.tsx
@@ -0,0 +1,26 @@
+import { ErrorTable } from "../ErrorTable";
+import { Text } from "../ui/text";
+import {
+  Response_Track_Axis_Error,
+  Response_Track_Driver_Error,
+  /*@ts-ignore Ignore git acticon type check */
+} from "../proto/mmc/info_pb";
+
+export type StatusPageProps = {
+  systemErrors: {
+    lineName: string;
+    axisErrors: Response_Track_Axis_Error[];
+    driverErrors: Response_Track_Driver_Error[];
+  }[];
+};
+
+export const StatusPage = (props: StatusPageProps) => {
+  return (
+    <>
+      <Text fontWeight="bold" marginLeft="1em" size="lg" marginBottom="0.5em">
+        Status
+      </Text>
+      <ErrorTable systemErrors={props.systemErrors} />
+    </>
+  );
+};
