e5bb8eeb966962f2f41d1b753e6d9e3822d666e4
kmtso434 <kmtso434@naver.com>
2025-09-19 17:47:51 +0900

    fix: Remove signal to not copy data

--- src/components/ErrorTable.tsx
+++ src/components/ErrorTable.tsx
@@ -1,6 +1,6 @@
-import { createSignal, JSX } from "solid-js";
+import { JSX } from "solid-js";
 import { Table } from "~/components/ui/table.tsx";
-import { For, createEffect, on } from "solid-js";
+import { For } from "solid-js";
 import { Text } from "./ui/text";
 import {
   Response_Track_Axis_Error,
@@ -8,11 +8,6 @@ import {
   /*@ts-ignore Ignore git acticon type check */
 } from "./proto/mmc/info_pb";
 import { Show } from "solid-js";
-
-type ErrorTable = {
-  location: string;
-  errorInfo: string[];
-};
 
 type ErrorTableProps = JSX.HTMLAttributes<HTMLDivElement> & {
   systemErrors: {
@@ -23,126 +18,29 @@ type ErrorTableProps = JSX.HTMLAttributes<HTMLDivElement> & {
   clearErrorAuto: boolean;
 };
 
-enum ErrorKind {
-  Driver = "Driver",
-  Axis = "Axis",
-}
-
 export const ErrorTable = (props: ErrorTableProps) => {
-  const systemErrors = () => props.systemErrors;
+  const systemError = () => {
+    return props.systemErrors;
+  };
 
-  createEffect(
-    on(
-      () => systemErrors(),
-      () => {
-        if (!systemErrors() || systemErrors().length < 1) {
-          setErrorTable([]);
-          return;
-        }
-        for (const { lineName, axisErrors, driverErrors } of systemErrors()) {
-          const prettierName = prettierLabel(lineName);
-          if (axisErrors) {
-            trackError(axisErrors, prettierName, ErrorKind.Axis);
-          }
-
-          if (props.clearErrorAuto) {
-            if (
-              errorTable().some((table) =>
-                table.location.includes(ErrorKind.Driver),
-              )
-            ) {
-              trackError(driverErrors, prettierName, ErrorKind.Driver);
-            } else {
-              continue;
-            }
-          } else {
-            if (driverErrors) {
-              trackError(driverErrors, prettierName, ErrorKind.Driver);
-            }
-          }
-        }
-      },
-      { defer: true },
-    ),
-  );
-
-  // Make driver axis to string enum
-  const trackError = (errors: object[], lineName: string, kind: ErrorKind) => {
-    errors.forEach((error) => {
-      const track = findError(error);
-      const hasError = track.length > 0;
-      const location = `${lineName} ${kind} ${error["id" as keyof typeof error]}`;
-      if (hasError) {
-        const newRow = {
-          location: location,
-          errorInfo: track,
-        };
-        addRow(newRow);
+  const showTable = () => {
+    const findSystemError = systemError().map((system) => {
+      const findAxisErrors = system.axisErrors
+        .map((axis) => Object.values(axis).includes(true))
+        .includes(true);
+      if (findAxisErrors) {
+        return true;
       } else {
-        if (hasRow(location)) {
-          deleteRow(location);
+        if (!props.clearErrorAuto) {
+          const findDriverErrors = system.driverErrors
+            .map((driver) => Object.values(driver).includes(true))
+            .includes(true);
+          return findDriverErrors;
         }
       }
+      return false;
     });
-  };
-
-  const findError = (error: object): string[] => {
-    const errorKeys: string[] = [];
-    const entries = Object.entries(error);
-    for (const [key, value] of entries) {
-      if (value) {
-        const errorInfo = prettierLabel(key);
-        if (typeof value === "boolean") {
-          errorKeys.push(errorInfo);
-        } else if (typeof value === "object") {
-          const track = findError(value).toString();
-          if (track.length > 0) {
-            errorKeys.push(`${errorInfo}: ${track}`);
-          }
-        }
-      }
-    }
-    return errorKeys;
-  };
-
-  const prettierLabel = (label: string) => {
-    return `${label[0].toUpperCase()}${label.slice(1, label.length)}`;
-  };
-
-  const [errorTable, setErrorTable] = createSignal<ErrorTable[]>([]);
-
-  const addRow = (row: ErrorTable) => {
-    const rowIndex = errorTable().findIndex(
-      (error) => error.location === row.location,
-    );
-    const rowFound = rowIndex !== -1;
-    if (rowFound) {
-      const currentErrorInfo = errorTable()[rowIndex].errorInfo;
-      const differentErrorInfo =
-        currentErrorInfo.toString() !== row.errorInfo.toString();
-      if (differentErrorInfo) {
-        setErrorTable((prev) => {
-          const updatePrev = prev.map((prevRow, i) => {
-            return i === rowIndex ? row : prevRow;
-          });
-          return updatePrev;
-        });
-      }
-    } else {
-      setErrorTable((prev) => [...prev, row]);
-    }
-  };
-
-  const deleteRow = (location: string) => {
-    return setErrorTable((prev) => {
-      const deleteRow = prev.filter((prevRow) => prevRow.location !== location);
-      return deleteRow;
-    });
-  };
-
-  const hasRow = (location: string) => {
-    const hasRow = errorTable().some((row) => row.location === location);
-    return hasRow;
+    return findSystemError.includes(true);
   };
 
   return (
@@ -157,7 +55,7 @@ export const ErrorTable = (props: ErrorTableProps) => {
         Error
       </Text>
       <Show
-        when={errorTable().length > 0}
+        when={showTable()}
         fallback={
           <Text color="fg.subtle" size="sm" marginLeft="1.2em">
             None errors.
@@ -173,27 +71,78 @@ export const ErrorTable = (props: ErrorTableProps) => {
           <Table.Head>
             <Table.Row>
               <Table.Header fontWeight="bold"> Location</Table.Header>
-              <Table.Header fontWeight="bold"> Error</Table.Header>
+              <Table.Header fontWeight="bold" color="red.9">
+                Error
+              </Table.Header>
             </Table.Row>
           </Table.Head>
           <Table.Body userSelect="none">
-            <For each={errorTable()}>
-              {(row) => (
-                <Table.Row style={{ padding: "0" }}>
-                  <Table.Cell style={{ height: "min-content", margin: "0" }}>
-                    <Text>{row.location}</Text>
-                  </Table.Cell>
-                  <Table.Cell>
-                    <For each={row.errorInfo}>
-                      {(info) => <Text>{info}</Text>}
-                    </For>
-                  </Table.Cell>
-                </Table.Row>
+            <For each={props.systemErrors}>
+              {(systemError) => (
+                <>
+                  <Show when={!props.clearErrorAuto}>
+                    <ErrorTableRows
+                      location={systemError.lineName}
+                      locationKind={LocationKind.Driver}
+                      errors={systemError.driverErrors}
+                    />
+                  </Show>
+                  <ErrorTableRows
+                    location={systemError.lineName}
+                    locationKind={LocationKind.Axis}
+                    errors={systemError.axisErrors}
+                  />
+                </>
               )}
             </For>
           </Table.Body>
         </Table.Root>
       </Show>
     </>
+  );
+};
+
+enum LocationKind {
+  Driver = "driver",
+  Axis = "axis",
+}
+
+type ErrorTableRowProps = {
+  errors: object[];
+  location: string;
+  locationKind: LocationKind;
+};
+
+const ErrorTableRows = (props: ErrorTableRowProps) => {
+  return (
+    <For each={props.errors}>
+      {(axis, i) => {
+        if (
+          Object.values(axis).some(
+            (value) => typeof value === "boolean" && value,
+          )
+        ) {
+          const keys = Object.entries(axis)
+            .filter((entry) => typeof entry[1] === "boolean" && entry[1])
+            .map((entry) => entry[0]);
+          const locationKindId = i() + 1;
+
+          return (
+            <Table.Row style={{ padding: "0" }}>
+              <Table.Cell style={{ height: "min-content", margin: "0" }}>
+                <Text>
+                  {`${props.location} ${props.locationKind} ${locationKindId}`}
+                </Text>
+              </Table.Cell>
+              <Table.Cell>
+                <For each={keys}>
+                  {(info) => <Text color="red.9">{info}</Text>}
+                </For>
+              </Table.Cell>
+            </Table.Row>
+          );
+        }
+      }}
+    </For>
   );
 };
