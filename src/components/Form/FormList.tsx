839e4b1a42c980ca2945233d298bf971b7d899ee
kmtso434 <122084476+kmtso434@users.noreply.github.com>
2025-10-02 18:06:51 +0900

    fix: Seperate Component

---
+++ src/components/Form/FormList.tsx
@@ -0,0 +1,186 @@
+import { Accordion } from "../ui/accordion";
+import { AccordionStatuses, LinkedStatuses, GainLockStatuses, ConfigObject } from "../ConfigForm";
+import { splitProps, createSignal, on, createEffect, For, Show } from "solid-js";
+import { Stack } from "styled-system/jsx";
+import { Tooltip } from "../ui/tooltip";
+import { createStore } from "solid-js/store";
+import { IconButton } from "../ui/icon-button";
+import { IconLink, IconLinkOff, IconChevronDown } from "@tabler/icons-solidjs";
+import { Text } from "../ui/text";
+
+export type FormListProps = Accordion.RootProps & {
+  id_prefix: string;
+  label: string;
+  items: object[];
+  onItemChange?: () => void;
+  accordionStatuses: AccordionStatuses;
+  linkedStatuses: LinkedStatuses;
+  gainLockStatuses: GainLockStatuses;
+  gainKinds: string[];
+};
+
+export function FormList(props: FormListProps) {
+  const [, rest] = splitProps(props, ["items"]);
+
+  const [items, setItems] = createStore<object[]>(props.items);
+
+  // Store a deep copy string of the most recently edited item object. This is
+  // necessary over storing e.g. the item index, as the signal that sets other
+  // items to be a copy cannot depend on the `items` store itself. Depending
+  // directly on the `items` store will cause an infinite effects loop.
+  const [recentEditedItem, setRecentEditedItem] = createSignal<string>("");
+
+  const changedItemIndex = props.linkedStatuses.get(props.label)?.[0]()[1];
+  if (changedItemIndex !== undefined) {
+    setRecentEditedItem(JSON.stringify(items[changedItemIndex]));
+  }
+
+  createEffect(
+    on(
+      () => JSON.stringify(items),
+      () => {
+        if (
+          props.linkedStatuses.get(props.label) &&
+          props.linkedStatuses.get(props.label)?.[0]()[1]
+        ) {
+          const index = props.linkedStatuses.get(props.label)![0]()[1];
+          setRecentEditedItem(JSON.stringify(items[index]));
+        }
+      },
+      { defer: true },
+    ),
+  );
+
+  createEffect(
+    on(
+      [
+        () => recentEditedItem(),
+        () => props.linkedStatuses.get(props.label)?.[0]()[0],
+      ],
+      () => {
+        if (!Array.isArray(items)) return;
+        if (props.linkedStatuses.get(props.label)?.[0]()[0]) {
+          items.forEach((_, index) => {
+            const item = JSON.parse(recentEditedItem().replaceAll("null", "0"));
+            if (index === props.linkedStatuses.get(props.label)?.[0]()[1]) {
+              return;
+            }
+            setItems(index, item);
+          });
+        }
+      },
+      { defer: true },
+    ),
+  );
+
+  // Converts label to have uppercase letters at the start of each word.
+  const prettifiedLabel = Array.from(props.label)
+    .map((char, i) => {
+      if (i === 0) return char.toUpperCase();
+      else if (props.label[i - 1] === "_") return char.toUpperCase();
+      else return char;
+    })
+    .toString()
+    .replaceAll(",", "");
+
+  return (
+    <Accordion.Root
+    style={{"border-width": "0"}}
+      multiple
+      {...rest}
+      value={props.accordionStatuses.get(props.label)?.[0]()}
+      onValueChange={(e) => {
+        props.accordionStatuses.get(props.label)?.[1](e.value);
+      }}
+    >
+      <For each={props.items}>
+        {(item, index) => {
+          const title = props.label + " " + (index() + 1).toString();
+          return (
+            <Accordion.Item value={title}>
+              <Stack direction="row">
+                <Tooltip.Root>
+                  <Tooltip.Trigger>
+                    <IconButton
+                      variant="ghost"
+                      onClick={() => {
+                        const linked = props.linkedStatuses.get(
+                          props.label,
+                        )?.[0]()[0];
+
+                        props.linkedStatuses.get(props.label)?.[1]([
+                          !linked,
+                          index(),
+                        ]);
+                        setRecentEditedItem(JSON.stringify(item));
+                      }}
+                      marginTop="0.5rem"
+                    >
+                      <Show
+                        when={props.linkedStatuses.get(props.label)?.[0]()[0]}
+                        fallback={<IconLinkOff />}
+                      >
+                        <IconLink />
+                      </Show>
+                    </IconButton>
+                  </Tooltip.Trigger>
+
+                  <Tooltip.Positioner>
+                    <Tooltip.Content backgroundColor="bg.default">
+                      <Text color="fg.default">
+                        Link {`${prettifiedLabel}`}
+                      </Text>
+                    </Tooltip.Content>
+                  </Tooltip.Positioner>
+                </Tooltip.Root>
+                <Accordion.ItemTrigger>
+                  <Text fontWeight="bold" size="md" color="fg.subtle">
+                    {`${title[0].toUpperCase()}${Array.from(
+                      title.slice(1, title.length),
+                    )
+                      .map((char, index) => {
+                        if (title[index] === "_") {
+                          return char.toUpperCase();
+                        }
+                        return char;
+                      })
+                      .toString()
+                      .replaceAll(",", "")}`}
+                  </Text>
+                  <Accordion.ItemIndicator>
+                    <IconChevronDown />
+                  </Accordion.ItemIndicator>
+                </Accordion.ItemTrigger>
+              </Stack>
+              <Accordion.ItemContent
+                paddingLeft="0.5rem"
+                paddingRight={"0.5rem"}
+              >
+                <ConfigObject
+                  object={item}
+                  id_prefix={props.id_prefix + title}
+                  onItemChange={() => {
+                    props.onItemChange?.();
+                    const linked = props.linkedStatuses.get(
+                      props.label,
+                    )?.[0]()[0];
+                    if (linked) {
+                      props.linkedStatuses.get(props.label)?.[1]([
+                        linked,
+                        index(),
+                      ]);
+                    }
+                  }}
+                  accordionStatuses={props.accordionStatuses}
+                  linkedStatuses={props.linkedStatuses}
+                  gainLockStatuses={props.gainLockStatuses}
+                  gainKinds={props.gainKinds}
+                />
+              </Accordion.ItemContent>
+            </Accordion.Item>
+          );
+        }}
+      </For>
+    </Accordion.Root>
+  );
+}
---
+++ src/components/Form/FormNumberInput.tsx
@@ -0,0 +1,156 @@
+import { Stack } from "styled-system/jsx";
+import { Text } from "../ui/text";
+import { GainLockStatuses } from "../ConfigForm";
+import { Show } from "solid-js";
+import {
+  IconExclamationCircle,
+  IconLock,
+  IconLockOff,
+} from "@tabler/icons-solidjs";
+import { IconButton } from "../ui/icon-button";
+
+export type FormNumberInputProps = {
+  id: string;
+  label: string;
+  lockStatus?: GainLockStatuses;
+  lockStatusKey?: string;
+  inputValue: number;
+  onInputChange?: (input: number) => void;
+};
+
+export const FormNumberInput = (props: FormNumberInputProps) => {
+  let divRef: HTMLDivElement | undefined;
+  const lockStatus = props.lockStatus;
+  const lockStatusKey = props.lockStatusKey;
+
+  function getComputedCSSVariableValue(variable: string) {
+    let value = getComputedStyle(document.documentElement).getPropertyValue(
+      variable,
+    );
+
+    while (value.startsWith("var(")) {
+      // Extract the name of the referenced variable
+      const referencedVarName = value.slice(4, value.length - 1);
+      value = getComputedStyle(document.documentElement).getPropertyValue(
+        referencedVarName,
+      );
+    }
+
+    return value.trim();
+  }
+
+  return (
+    <Stack direction="row" width="100%" marginTop="1rem" marginBottom="0.5rem">
+      <Text width="50%" marginTop="0.4rem" fontWeight="light">
+        {props.label}
+      </Text>
+      <div style={{ width: "50%" }}>
+        <Stack
+          ref={divRef}
+          style={{
+            width: "100%",
+            padding: "0.4rem",
+            "padding-right": "0.2rem",
+            "border-radius": "0.5rem",
+            "border-width": "1px",
+            gap: "0",
+          }}
+          borderColor={
+            Number.isFinite(Number(props.inputValue)) ? "bg.disabled" : "red"
+          }
+          direction="row"
+        >
+          <input
+            style={{
+              width:
+                lockStatus && lockStatusKey && lockStatus.has(lockStatusKey)
+                  ? `calc(100% - 2rem)`
+                  : "100%",
+              outline: "none",
+              opacity:
+                lockStatus && lockStatusKey && lockStatus.has(lockStatusKey)
+                  ? lockStatus.get(lockStatusKey)![0]()
+                    ? "0.4"
+                    : "1"
+                  : "1",
+            }}
+            disabled={
+              lockStatus && lockStatusKey && lockStatus.has(lockStatusKey)
+                ? lockStatus.get(lockStatusKey)![0]()
+                : false
+            }
+            onFocusIn={() => {
+              divRef!.style.borderWidth = "2px";
+              divRef!.style.borderColor = getComputedCSSVariableValue(
+                "--colors-accent-default",
+              );
+            }}
+            onFocusOut={(e) => {
+              divRef!.style.borderWidth = "1px";
+              divRef!.style.borderColor = !Number.isFinite(
+                Number(e.target.value),
+              )
+                ? "red"
+                : getComputedCSSVariableValue("--colors-bg-disabled");
+            }}
+            placeholder={props.label}
+            value={props.inputValue}
+            onChange={(e) => {
+              props.onInputChange?.(Number(e.target.value));
+            }}
+          />
+
+          <Show when={!Number.isFinite(props.inputValue)}>
+            <IconExclamationCircle color="red" data-name="config_field_error" />
+          </Show>
+          <Show
+            when={lockStatus && lockStatusKey && lockStatus.has(lockStatusKey)}
+          >
+            <IconButton
+              type="button"
+              variant="ghost"
+              size="sm"
+              borderRadius="3rem"
+              height="min-content"
+              paddingTop="0.2rem"
+              paddingBottom="0.2rem"
+              opacity={lockStatus!.get(lockStatusKey!)![0]() ? "1" : "0.5"}
+              onClick={() => {
+                if (!lockStatus) return;
+                lockStatus.get(lockStatusKey!)![1](
+                  !lockStatus.get(lockStatusKey!)![0](),
+                );
+
+                const split = lockStatusKey!.split(".");
+                const includingKeys = `${split[0]}.${split[1]}.`;
+                const mapValues = Array.from(lockStatus.entries())
+                  .filter((entries) => entries[0].includes(includingKeys))
+                  .map((entries) => entries[1][0]());
+                const parseValues = [...new Set(mapValues)];
+                if (parseValues.length !== 1) {
+                  lockStatus.get(includingKeys.slice(0, -1))![1](true);
+                } else {
+                  lockStatus.get(includingKeys.slice(0, -1))![1](
+                    parseValues[0],
+                  );
+                }
+              }}
+            >
+              <Show
+                when={lockStatus && lockStatus.get(lockStatusKey!)![0]()}
+                fallback={<IconLockOff />}
+              >
+                <IconLock />
+              </Show>
+            </IconButton>
+          </Show>
+        </Stack>
+        <Show when={!Number.isFinite(props.inputValue)}>
+          <Text size="sm" color="red">
+            {`Invalid ${typeof props.inputValue}.`}
+          </Text>
+        </Show>
+      </div>
+    </Stack>
+  );
+};
