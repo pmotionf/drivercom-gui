d3035676b4de1f019e1e763ccaa23ed8e68b3517
kmtso434 <122084476+kmtso434@users.noreply.github.com>
2025-10-10 17:44:27 +0900

    fix: Adapt form component at config page

--- src/components/ConfigForm.tsx
+++ src/components/ConfigForm.tsx
@@ -1,20 +1,6 @@
-import {
-  type Accessor,
-  createEffect,
-  createSignal,
-  For,
-  JSX,
-  on,
-  type Setter,
-} from "solid-js";
+import { type Accessor, createEffect, JSX, on, type Setter } from "solid-js";
 import { createStore } from "solid-js/store";
-
-import { Stack } from "styled-system/jsx";
-import { Text } from "./ui/text.tsx";
-import { FormCheckBox } from "./Form/FormCheckBox.tsx";
-import { FormNumberInput } from "./Form/FormNumberInput.tsx";
-import { FormCollapsibleObject } from "./Form/FormCollapsibleObject.tsx";
-import { FormList } from "./Form/FormList.tsx";
+import { AccordionStatuses, Form } from "./Form";
 
 export type ConfigFormProps = JSX.HTMLAttributes<HTMLFormElement> & {
   id: string;
@@ -23,11 +9,6 @@ export type ConfigFormProps = JSX.HTMLAttributes<HTMLFormElement> & {
   linkedStatuses: LinkedStatuses;
   gainLockStatuses: GainLockStatuses;
 };
-
-export type AccordionStatuses = Map<
-  string,
-  [Accessor<string[]>, Setter<string[]>]
->;
 
 export type LinkedStatuses = Map<
   string,
@@ -317,7 +298,7 @@ export function ConfigForm(props: ConfigFormProps) {
   }
 
   return (
-    <ConfigObject
+    <Form
       object={config}
       id_prefix={props.id}
       accordionStatuses={props.accordionStatuses}
@@ -325,203 +306,5 @@ export function ConfigForm(props: ConfigFormProps) {
       gainLockStatuses={props.gainLockStatuses}
       gainKinds={dynamic}
     />
-  );
-}
-
-type ConfigObjectProps = JSX.HTMLAttributes<HTMLDivElement> & {
-  id_prefix: string;
-  object: object;
-  onItemChange?: () => void;
-  accordionStatuses: AccordionStatuses;
-  linkedStatuses: LinkedStatuses;
-  gainLockStatuses: GainLockStatuses;
-  gainKinds: string[];
-  gainKey?: string;
-};
-
-export function ConfigObject(props: ConfigObjectProps) {
-  const [object, setObject] = createStore(props.object);
-
-  return (
-    <div>
-      <For each={Object.entries(object)}>
-        {(entry) => {
-          const key = entry[0];
-          const value = entry[1];
-          if (value.constructor === Array) {
-            if (!props.accordionStatuses.has(key)) {
-              props.accordionStatuses.set(key, createSignal<string[]>([]));
-            }
-            if (!props.linkedStatuses.has(key)) {
-              props.linkedStatuses.set(
-                key,
-                createSignal<[boolean, number]>([false, 0]),
-              );
-            }
-            return (
-              <>
-                <Stack
-                  style={{
-                    "border-width": "1px",
-                    "padding-right": "1rem",
-                    "padding-left": "1rem",
-                    "margin-top": "1rem",
-                    "border-radius": "0.5rem",
-                    "margin-bottom": "0.5rem",
-                    "padding-bottom": "0.5rem",
-                  }}
-                >
-                  <FormList
-                    label={key}
-                    items={value}
-                    onItemChange={() => props.onItemChange?.()}
-                    id_prefix={props.id_prefix}
-                    accordionStatuses={props.accordionStatuses}
-                    linkedStatuses={props.linkedStatuses}
-                    gainLockStatuses={props.gainLockStatuses}
-                    gainKinds={props.gainKinds}
-                  />
-                </Stack>
-              </>
-            );
-          }
-          if (typeof value === "object") {
-            let gainkey = props.gainKey ? props.gainKey : "";
-            if (gainkey.length !== 0) {
-              gainkey = `${props.gainKey}.${key}`;
-            }
-
-            const index = props.gainKinds.indexOf(key.toLowerCase());
-
-            if (index !== -1) {
-              gainkey = `${props.gainKinds[index]}`;
-              if (
-                !props.gainLockStatuses.has(gainkey) &&
-                Object.keys(value).includes("gain")
-              ) {
-                props.gainLockStatuses.set(
-                  `${gainkey}.gain`,
-                  createSignal<boolean>(false),
-                );
-              }
-            }
-
-            if (
-              Object.values(value).length > 1 ||
-              Object.values(value).some((val) => typeof val === "object")
-            ) {
-              return (
-                <FormCollapsibleObject
-                  id_prefix={props.id_prefix}
-                  key={key}
-                  object={value}
-                  gainKey={gainkey}
-                  gainKinds={props.gainKinds}
-                  accordionStatuses={props.accordionStatuses}
-                  linkedStatuses={props.linkedStatuses}
-                  gainLockStatuses={props.gainLockStatuses}
-                  onItemChange={() => props.onItemChange?.()}
-                />
-              );
-            } else {
-              return (
-                <>
-                  <div
-                    style={{
-                      "border-width": "1px",
-                      padding: "0.5rem 1rem 0.5rem 1rem",
-                      "margin-top": "0.5rem",
-                      "border-radius": "0.5em",
-                    }}
-                  >
-                    <Text fontWeight="bold" color="fg.subtle">
-                      {`${key[0].toUpperCase()}${Array.from(
-                        key.slice(1, key.length),
-                      )
-                        .map((char, index) => {
-                          if (key[index] === "_") {
-                            return char.toUpperCase();
-                          }
-                          return char;
-                        })
-                        .toString()
-                        .replaceAll(",", "")}`}
-                    </Text>
-
-                    <ConfigObject
-                      object={value}
-                      id_prefix={props.id_prefix + key}
-                      style={{ "padding-left": "1rem" }}
-                      onItemChange={() => {
-                        props.onItemChange?.();
-                      }}
-                      accordionStatuses={props.accordionStatuses}
-                      linkedStatuses={props.linkedStatuses}
-                      gainLockStatuses={props.gainLockStatuses}
-                      gainKinds={props.gainKinds}
-                      gainKey={gainkey}
-                    />
-                  </div>
-                </>
-              );
-            }
-          }
-          if (typeof value === "boolean") {
-            return (
-              <FormCheckBox
-                id={props.id_prefix + key}
-                label={key}
-                checked={object[key as keyof typeof object]}
-                onCheckedChange={(checked) =>
-                  setObject(
-                    key as keyof typeof object,
-                    // @ts-ignore: TSC unable to handle generic object type
-                    // in store
-                    checked,
-                  )
-                }
-              />
-            );
-          }
-          if (typeof value === "number") {
-            let lockStatusKey = "";
-            const lockStatus = props.gainLockStatuses;
-            if (
-              props.gainKey &&
-              props.gainKey.length !== 0 &&
-              !props.gainKinds.includes(props.gainKey)
-            ) {
-              if (key.toLowerCase() === "p" || key.toLowerCase() === "i") {
-                lockStatusKey = `${props.gainKey}.${key}`;
-                if (!lockStatus.has(lockStatusKey)) {
-                  lockStatus.set(lockStatusKey, createSignal<boolean>(false));
-                }
-              }
-            }
-
-            return (
-              <FormNumberInput
-                id={props.id_prefix + key}
-                label={key}
-                lockStatus={lockStatus}
-                lockStatusKey={
-                  lockStatusKey.length > 0 ? lockStatusKey : undefined
-                }
-                inputValue={object[key as keyof typeof object]}
-                onInputChange={(value) => {
-                  setObject(
-                    key as keyof typeof object,
-                    // @ts-ignore: TSC unable to handle generic object type
-                    // in store
-                    value,
-                  );
-                  props.onItemChange?.();
-                }}
-              />
-            );
-          }
-        }}
-      </For>
-    </div>
   );
 }
