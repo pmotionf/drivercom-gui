4fec0ec6508c618d6020bde560dafc678c0da31e
kmtso434 <122084476+kmtso434@users.noreply.github.com>
2025-11-04 18:06:05 +0900

    store changes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -13,6 +13,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_tcp::init())
+        .plugin(tauri_plugin_network::init())
         .invoke_handler(tauri::generate_handler![version])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
