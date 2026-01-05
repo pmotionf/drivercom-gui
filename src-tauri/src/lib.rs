aa249b7f1578107abe1afbbaa63d4244d1c7e5a7
kmotso434 <kmtso434@naver.com>
2026-01-05 17:31:44 +0900

    scm: Add Pty terminal

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -39,6 +39,7 @@ pub fn run() {
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_tcp::init())
+        .plugin(tauri_plugin_pty::init())
         .invoke_handler(tauri::generate_handler![version, get_server_addrs])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
