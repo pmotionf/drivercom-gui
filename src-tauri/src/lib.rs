7493db0e7576f9b94f604bb263ebbe7a798dbcd9
kmotso434 <kmtso434@naver.com>
2026-01-05 18:23:43 +0900

    dep: tauri platform

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -33,6 +33,7 @@ async fn get_server_addrs() -> Vec<String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_window_state::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
