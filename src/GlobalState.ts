66db23a5d0290fd840818e976665a2a06b319b75
kmotso434 <kmtso434@naver.com>
2026-01-13 16:41:48 +0900

    feat: Show api version in side bar

--- src/GlobalState.ts
+++ src/GlobalState.ts
@@ -30,6 +30,7 @@ export const GlobalStateContext = createContext<{
 
 export const [cliVersion, setCliVersion] = createSignal<string>("");
 export const [driverComVersion, setDriverComVersion] = createSignal<string>("");
+export const [apiVersion, setApiVersion] = createSignal<string>("");
 
 export const [portList, setPortList] = createSignal<Port[]>([]);
 
