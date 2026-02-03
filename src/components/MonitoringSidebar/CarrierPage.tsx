c749bfe78b3910ac959ede713455803a2813956b
kmotso434 <kmtso434@naver.com>
2026-02-03 11:27:46 +0900

    fix: Show carrier state changed in real time

--- src/components/MonitoringSidebar/CarrierPage.tsx
+++ src/components/MonitoringSidebar/CarrierPage.tsx
@@ -8,6 +8,8 @@ import { ToggleGroup } from "../ui/toggle-group";
 import { Button } from "../ui/button";
 import { Select } from "../ui/select";
 import { createListCollection, ListCollection } from "@ark-ui/solid";
+import { SendingCommand } from "../System/System";
+import { Show } from "solid-js";
 
 export type CarrierPageProps = JSX.HTMLAttributes<HTMLDivElement> & {
   carrierStates: CarrierState[];
@@ -18,6 +20,7 @@ export type CarrierPageProps = JSX.HTMLAttributes<HTMLDivElement> & {
     controlMode?: string,
     cas?: string,
   ) => void;
+  sendingCommand: SendingCommand | null;
 };
 
 export type CarrierState = {
@@ -46,6 +49,7 @@ export function CarrierPage(props: CarrierPageProps) {
     new Map();
   const targetValueMap: Map<string, [Accessor<string>, Setter<string>]> =
     new Map();
+  const [lastCommandKey, setLastCommandKey] = createSignal<string>("");
 
   const targetKindList = [
     { label: TargetKind.Axis, value: TargetKind.Axis },
@@ -62,198 +66,224 @@ export function CarrierPage(props: CarrierPageProps) {
     items: targetKindList,
   });
 
+  const carrierStates = () => {
+    return props.carrierStates;
+  };
+
   return (
     <div style={{ width: "100%", height: "100%" }}>
       <Accordion.Root width="100%" height={"100%"} multiple borderWidth={"0"}>
-        <For each={props.carrierStates}>
+        <For each={carrierStates()}>
           {(carrier) => {
-            if (carrier.carrierStates.length < 1) return;
             return (
-              <Accordion.Item
-                value={carrier.lineName}
-                borderBottomWidth={"1px"}
-              >
-                <Accordion.ItemTrigger justifyContent="left">
-                  <Accordion.ItemIndicator>
-                    <IconChevronDown />
-                  </Accordion.ItemIndicator>
-                  {carrier.lineName}
-                </Accordion.ItemTrigger>
+              <Show when={carrier.carrierStates.length >= 1}>
+                <Accordion.Item
+                  value={carrier.lineName}
+                  borderBottomWidth={"1px"}
+                >
+                  <Accordion.ItemTrigger justifyContent="left">
+                    <Accordion.ItemIndicator>
+                      <IconChevronDown />
+                    </Accordion.ItemIndicator>
+                    {carrier.lineName}
+                  </Accordion.ItemTrigger>
 
-                <Accordion.ItemContent>
-                  <For each={carrier.carrierStates}>
-                    {(carrierState) => {
-                      const mapKey = `${carrier.lineName}${carrierState.id}`;
-                      if (!casMap.has(mapKey)) {
-                        casMap.set(
-                          mapKey,
-                          createSignal<boolean>(!carrierState.casDisabled),
-                        );
-                      }
-                      const [cas, setCas] = casMap.get(mapKey)!;
+                  <Accordion.ItemContent>
+                    <For each={carrier.carrierStates}>
+                      {(carrierState) => {
+                        const mapKey = `${carrier.lineName}${carrierState.id}`;
+                        if (!casMap.has(mapKey)) {
+                          casMap.set(
+                            mapKey,
+                            createSignal<boolean>(!carrierState.casDisabled),
+                          );
+                        }
+                        const [cas, setCas] = casMap.get(mapKey)!;
 
-                      if (!controlModeMap.has(mapKey)) {
-                        controlModeMap.set(
-                          mapKey,
-                          createSignal<ControlMode>(ControlMode.Position),
-                        );
-                      }
-                      const [controlMode, setControlMode] =
-                        controlModeMap.get(mapKey)!;
+                        if (!controlModeMap.has(mapKey)) {
+                          controlModeMap.set(
+                            mapKey,
+                            createSignal<ControlMode>(ControlMode.Position),
+                          );
+                        }
+                        const [controlMode, setControlMode] =
+                          controlModeMap.get(mapKey)!;
 
-                      if (!targetKindMap.has(mapKey)) {
-                        targetKindMap.set(
-                          mapKey,
-                          createSignal<TargetKind>(TargetKind.Axis),
-                        );
-                      }
-                      const [targetKind, setTargetKind] =
-                        targetKindMap.get(mapKey)!;
+                        if (!targetKindMap.has(mapKey)) {
+                          targetKindMap.set(
+                            mapKey,
+                            createSignal<TargetKind>(TargetKind.Axis),
+                          );
+                        }
+                        const [targetKind, setTargetKind] =
+                          targetKindMap.get(mapKey)!;
 
-                      if (!targetValueMap.has(mapKey)) {
-                        targetValueMap.set(mapKey, createSignal<string>(""));
-                      }
-                      const [targetValue, setTargetValue] =
-                        targetValueMap.get(mapKey)!;
+                        if (!targetValueMap.has(mapKey)) {
+                          targetValueMap.set(mapKey, createSignal<string>(""));
+                        }
+                        const [targetValue, setTargetValue] =
+                          targetValueMap.get(mapKey)!;
 
-                      return (
-                        <div
-                          style={{
-                            display: "grid",
-                            "grid-auto-columns": `14rem 8rem 3rem 3rem`,
-                            "column-gap": "0.5rem",
-                            "row-gap": "0.5rem",
-                            "grid-auto-rows": `1rem 1rem 2.5rem`,
-                            "margin-bottom": "1rem",
-                          }}
-                        >
-                          <Text
-                            fontSize="md"
-                            fontWeight="bold"
-                            style={{ "grid-row": 1, "grid-column": 1 }}
-                          >
-                            {`Carrier ${carrierState.id}`}
-                          </Text>
-                          <Text style={{ "grid-row": 2, "grid-column": 1 }}>
-                            {"Target"}
-                          </Text>
+                        return (
                           <div
                             style={{
-                              display: "flex",
-                              "grid-row": 3,
-                              "grid-column": 1,
-                              width: "100%",
+                              display: "grid",
+                              "grid-auto-columns": `14rem 8rem 3rem 3rem`,
+                              "column-gap": "0.5rem",
+                              "row-gap": "0.5rem",
+                              "grid-auto-rows": `1rem 1rem 2.5rem`,
+                              "margin-bottom": "1rem",
                             }}
                           >
-                            <Input
-                              value={targetValue()}
-                              onChange={(e) => setTargetValue(e.target.value)}
+                            <Text
+                              fontSize="md"
+                              fontWeight="bold"
+                              style={{ "grid-row": 1, "grid-column": 1 }}
+                            >
+                              {`Carrier ${carrierState.id}`}
+                            </Text>
+                            <Text style={{ "grid-row": 2, "grid-column": 1 }}>
+                              {"Target"}
+                            </Text>
+                            <div
                               style={{
-                                width: "50%",
-                                "border-top-right-radius": "0rem",
-                                "border-bottom-right-radius": "0rem",
+                                display: "flex",
+                                "grid-row": 3,
+                                "grid-column": 1,
+                                width: "100%",
                               }}
-                            />
-                            <Select.Root
-                              positioning={{ sameWidth: true }}
-                              width="sm"
-                              collection={targetKindCollection}
-                              defaultValue={[targetKind()]}
-                              onValueChange={(details) => {
-                                setTargetKind(
-                                  TargetKind[
-                                    details.items[0]
-                                      .label as keyof typeof TargetKind
+                            >
+                              <Input
+                                value={targetValue()}
+                                onChange={(e) => setTargetValue(e.target.value)}
+                                style={{
+                                  width: "50%",
+                                  "border-top-right-radius": "0rem",
+                                  "border-bottom-right-radius": "0rem",
+                                }}
+                              />
+                              <Select.Root
+                                positioning={{ sameWidth: true }}
+                                width="sm"
+                                collection={targetKindCollection}
+                                defaultValue={[targetKind()]}
+                                onValueChange={(details) => {
+                                  setTargetKind(
+                                    TargetKind[
+                                      details.items[0]
+                                        .label as keyof typeof TargetKind
+                                    ],
+                                  );
+                                }}
+                              >
+                                <Select.Control>
+                                  <Select.Trigger
+                                    width="6rem"
+                                    borderLeftWidth={"0px"}
+                                    borderTopLeftRadius={"0rem"}
+                                    borderBottomLeftRadius={"0rem"}
+                                  >
+                                    <Select.ValueText
+                                      placeholder={`Select target`}
+                                    />
+                                  </Select.Trigger>
+                                </Select.Control>
+                                <Select.Positioner>
+                                  <Select.Content>
+                                    <For each={targetKindCollection.items}>
+                                      {(item) => (
+                                        <Select.Item item={item}>
+                                          <Select.ItemText>
+                                            {item.label}
+                                          </Select.ItemText>
+                                          <Select.ItemIndicator></Select.ItemIndicator>
+                                        </Select.Item>
+                                      )}
+                                    </For>
+                                  </Select.Content>
+                                </Select.Positioner>
+                              </Select.Root>
+                            </div>
+
+                            <Text style={{ "grid-row": 2, "grid-column": 2 }}>
+                              {"Control Mode"}
+                            </Text>
+                            <ToggleGroup.Root
+                              style={{ "grid-row": 3, "grid-column": 2 }}
+                              value={[controlMode()]}
+                              onValueChange={(details) =>
+                                setControlMode(
+                                  ControlMode[
+                                    details.value[0] as keyof typeof ControlMode
                                   ],
+                                )
+                              }
+                            >
+                              <ToggleGroup.Item
+                                value={ControlMode.Position}
+                                width={"50%"}
+                              >
+                                {"Position"}
+                              </ToggleGroup.Item>
+                              <ToggleGroup.Item
+                                value={ControlMode.Velocity}
+                                width={"50%"}
+                              >
+                                {"Velocity"}
+                              </ToggleGroup.Item>
+                            </ToggleGroup.Root>
+                            <Text style={{ "grid-row": 2, "grid-column": 3 }}>
+                              {"CAS"}
+                            </Text>
+                            <Button
+                              variant={cas() ? "solid" : "outline"}
+                              style={{ "grid-row": 3, "grid-column": 3 }}
+                              onClick={() => setCas(!cas())}
+                            >
+                              {cas() ? "on" : "off"}
+                            </Button>
+                            <Button
+                              style={{ "grid-row": 3, "grid-column": 4 }}
+                              disabled={
+                                props.sendingCommand
+                                  ? props.sendingCommand.line ===
+                                    carrier.lineName
+                                    ? props.sendingCommand.movingCarrier &&
+                                      lastCommandKey() === mapKey
+                                      ? false
+                                      : true
+                                    : true
+                                  : false
+                              }
+                              loading={
+                                props.sendingCommand &&
+                                props.sendingCommand!.line ===
+                                  carrier.lineName &&
+                                props.sendingCommand!.movingCarrier &&
+                                lastCommandKey() === mapKey
+                                  ? true
+                                  : false
+                              }
+                              onClick={() => {
+                                setLastCommandKey(mapKey);
+                                props.onCarrierMove?.(
+                                  carrier.lineName,
+                                  carrierState.id,
+                                  `${targetValue()}${targetKind()}`,
+                                  controlMode(),
+                                  cas() ? undefined : "off",
                                 );
                               }}
                             >
-                              <Select.Control>
-                                <Select.Trigger
-                                  width="6rem"
-                                  borderLeftWidth={"0px"}
-                                  borderTopLeftRadius={"0rem"}
-                                  borderBottomLeftRadius={"0rem"}
-                                >
-                                  <Select.ValueText
-                                    placeholder={`Select target`}
-                                  />
-                                </Select.Trigger>
-                              </Select.Control>
-                              <Select.Positioner>
-                                <Select.Content>
-                                  <For each={targetKindCollection.items}>
-                                    {(item) => (
-                                      <Select.Item item={item}>
-                                        <Select.ItemText>
-                                          {item.label}
-                                        </Select.ItemText>
-                                        <Select.ItemIndicator></Select.ItemIndicator>
-                                      </Select.Item>
-                                    )}
-                                  </For>
-                                </Select.Content>
-                              </Select.Positioner>
-                            </Select.Root>
+                              {"Move"}
+                            </Button>
                           </div>
-
-                          <Text style={{ "grid-row": 2, "grid-column": 2 }}>
-                            {"Control Mode"}
-                          </Text>
-                          <ToggleGroup.Root
-                            style={{ "grid-row": 3, "grid-column": 2 }}
-                            value={[controlMode()]}
-                            onValueChange={(details) =>
-                              setControlMode(
-                                ControlMode[
-                                  details.value[0] as keyof typeof ControlMode
-                                ],
-                              )
-                            }
-                          >
-                            <ToggleGroup.Item
-                              value={ControlMode.Position}
-                              width={"50%"}
-                            >
-                              {"Position"}
-                            </ToggleGroup.Item>
-                            <ToggleGroup.Item
-                              value={ControlMode.Velocity}
-                              width={"50%"}
-                            >
-                              {"Velocity"}
-                            </ToggleGroup.Item>
-                          </ToggleGroup.Root>
-                          <Text style={{ "grid-row": 2, "grid-column": 3 }}>
-                            {"CAS"}
-                          </Text>
-                          <Button
-                            variant={cas() ? "solid" : "outline"}
-                            style={{ "grid-row": 3, "grid-column": 3 }}
-                            onClick={() => setCas(!cas())}
-                          >
-                            {cas() ? "on" : "off"}
-                          </Button>
-                          <Button
-                            style={{ "grid-row": 3, "grid-column": 4 }}
-                            onClick={() => {
-                              props.onCarrierMove?.(
-                                carrier.lineName,
-                                carrierState.id,
-                                `${targetValue()}${targetKind()}`,
-                                controlMode(),
-                                cas() ? undefined : "off",
-                              );
-                            }}
-                          >
-                            {"Move"}
-                          </Button>
-                        </div>
-                      );
-                    }}
-                  </For>
-                </Accordion.ItemContent>
-              </Accordion.Item>
+                        );
+                      }}
+                    </For>
+                  </Accordion.ItemContent>
+                </Accordion.Item>
+              </Show>
             );
           }}
         </For>
