0aff585bd8da79727aebb1006bdd1eb11cb211a1
kmotso434 <kmtso434@naver.com>
2026-02-25 09:28:29 +0900

    fix: Run Css test in jsdom environment

--- tests/utils/GetComputedCssVariableValue.test.ts
+++ tests/utils/GetComputedCssVariableValue.test.ts
@@ -1,6 +1,18 @@
-import { expect, test } from "vitest";
+import { expect, describe, it } from "vitest";
 import { getComputedCSSVariableValue } from "../../src/utils/GetComputedCssVariableValue";
 
-test("Test get computed CSS variable value", () => {
-  expect(getComputedCSSVariableValue("var(--bg-disabled)")).toBe("");
+// @vitest-environment jsdom
+describe("getComputedCSSVariableValue", () => {
+  it("returns resolved css variable value", () => {
+    document.documentElement.style.setProperty("--color", "red");
+    const result = getComputedCSSVariableValue("--color");
+    expect(result).toBe("red");
+  });
+
+  it("resolves nested var()", () => {
+    document.documentElement.style.setProperty("--primary", "blue");
+    document.documentElement.style.setProperty("--color", "var(--primary)");
+    const result = getComputedCSSVariableValue("--color");
+    expect(result).toBe("blue");
+  });
 });
