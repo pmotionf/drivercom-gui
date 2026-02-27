5e3e6a076bd7f82f69e503d7abfc5cc85de0260c
kmotso434 <kmtso434@naver.com>
2026-02-27 15:58:58 +0900

    feat: Show overlay when file is drag over

--- src/components/Panel/PanelLayout.tsx
+++ src/components/Panel/PanelLayout.tsx
@@ -10,6 +10,7 @@ import { PanelLayoutProvider } from "./PanelLayoutProvier.tsx";
 export type PanelSizeContext = {
   id: string;
   size: number;
+  isFileDrop?: boolean;
 };
 
 export type PanelLayoutProps = JSX.HTMLAttributes<HTMLDivElement> & {
