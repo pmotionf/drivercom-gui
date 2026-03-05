3851e2cf402051e3d84ce63b6bedf57077103619
kmotso434 <kmtso434@naver.com>
2026-03-05 13:42:18 +0900

    fix: Clean up property type

--- src/components/Panel/PanelLayout.tsx
+++ src/components/Panel/PanelLayout.tsx
@@ -10,7 +10,6 @@ import { PanelLayoutProvider } from "./PanelLayoutProvider.tsx";
 export type PanelSizeContext = {
   id: string;
   size: number;
-  isDragLeave?: boolean;
 };
 
 export type PanelLayoutProps = JSX.HTMLAttributes<HTMLDivElement> & {
