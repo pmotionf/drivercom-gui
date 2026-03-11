f9baeebe2e990d1441ae24d07f9318ed8de958ec
kmotso434 <kmtso434@naver.com>
2026-03-11 11:31:18 +0900

    refactor: Adapt new type

--- src/pages/LogViewer/LogViewerTabPageContent.tsx
+++ src/pages/LogViewer/LogViewerTabPageContent.tsx
@@ -97,6 +97,7 @@ export function LogViewerTabPageContent() {
           filePath: newFilePath,
         },
         configTabPage: null,
+        loggingTabPage: null,
       },
     };
     setTimeout(() => {
