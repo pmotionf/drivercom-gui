29be240ffda4d02ea40563674a30dc3913ae5cae
kmotso434 <kmtso434@naver.com>
2026-03-19 11:44:13 +0900

    fix: Show description outside

--- src/components/Form/FormCheckBox.tsx
+++ src/components/Form/FormCheckBox.tsx
@@ -1,7 +1,5 @@
-import { IconHelp } from "@tabler/icons-solidjs";
 import { Checkbox } from "../ui/checkbox";
 import { Text } from "../ui/text";
-import { Tooltip } from "../ui/tooltip";
 import { Show } from "solid-js";
 
 export type FormCheckBoxProps = {
@@ -46,37 +44,33 @@ export const FormCheckBox = (props: FormCheckBoxProps) => {
         marginRight="0.5em"
         alignItems={"center"}
       >
-        <Text
-          fontWeight="medium"
-          userSelect="none"
-          paddingLeft="0.1em"
-          paddingRight="0.1em"
-          borderColor="accent.7"
-          borderBottomWidth={
-            props.originalValue !== props.checked ? "2px" : "0px"
-          }
-        >
-          {key}
-        </Text>
-      </Checkbox>
-      <Show
-        when={
-          props.desc &&
-          typeof props.desc["description" as keyof typeof props.desc] ===
-            "string"
-        }
-      >
-        <Tooltip.Root>
-          <Tooltip.Trigger marginTop="0.3rem">
-            <IconHelp size="1em" opacity={0.5} />
-          </Tooltip.Trigger>
-          <Tooltip.Positioner>
-            <Tooltip.Content>
+        <div>
+          <Text
+            fontWeight="medium"
+            userSelect="none"
+            paddingLeft="0.1em"
+            paddingRight="0.1em"
+            borderColor="accent.7"
+            borderBottomWidth={
+              props.originalValue !== props.checked ? "2px" : "0px"
+            }
+            width="min-content"
+          >
+            {key}
+          </Text>
+          <Show
+            when={
+              props.desc &&
+              typeof props.desc["description" as keyof typeof props.desc] ===
+                "string"
+            }
+          >
+            <Text size="xs" fontWeight="light">
               {props.desc!["description" as keyof typeof props.desc]}
-            </Tooltip.Content>
-          </Tooltip.Positioner>
-        </Tooltip.Root>
-      </Show>
+            </Text>
+          </Show>
+        </div>
+      </Checkbox>
     </div>
   );
 };
