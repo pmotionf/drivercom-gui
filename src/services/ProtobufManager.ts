9873c9f36a5ff6e0743db818faf07f0938d62307
kmotso434 <kmtso434@naver.com>
2026-08-26 10:16:38 +0900

    fix: Seperate protobuf module from server handler

---
+++ src/services/ProtobufManager.ts
@@ -0,0 +1,13 @@
+import { fromBinary, toBinary } from "@bufbuild/protobuf";
+import { Request, RequestSchema, Response, ResponseSchema } from "~/proto/mmc_pb";
+
+export class ProtobufManager {
+  decode(buffer: ArrayBuffer): Response {
+    const uint8Array = new Uint8Array(buffer)
+    return fromBinary(ResponseSchema, uint8Array)
+  }
+
+  encode(payload: Request): Uint8Array {
+    return toBinary(RequestSchema, payload)
+  }
+}
